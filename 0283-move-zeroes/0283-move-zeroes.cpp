b0250413f9dda36915bb1ac9b30895b7ba9307de
Himanshu Sisodiya <hsisodia988766@gmail.com>
2024-10-05 19:41:04 +0530

    Time: 887 ms (5.09%), Space: 22 MB (27.12%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+       for(int i=0;i<n;i++){
+        for(int j=0;j<n-1;j++){
+            if(nums[j]==0 && nums[j+1]!=0) swap(nums[j],nums[j+1]);
+        }
+       }
+    }
+};
