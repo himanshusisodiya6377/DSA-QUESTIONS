465d543cb3c2fbaadd57d0c81d7ce2c31cebf6b9
Himanshu Sisodiya <hsisodia988766@gmail.com>
2024-10-16 10:11:07 +0530

    Time: 3 ms (36.63%), Space: 8.5 MB (60.33%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -3,32 +3,33 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int r=matrix.size(); //rows
         int c=matrix[0].size(); //coloumn
+        int count=0;
+        int num=r*c;
         vector<int>v;
-           int minr=0,minc=0,maxr=r-1,maxc=c-1;
-           int count =0;
-   while((minr<=maxr) && (minc<=maxc)){
-
-        for(int j=minc;j<=maxc && count<r*c;j++){
-            v.push_back(matrix[minr][j]);
-            count++;
-        }
-        minr++;
-        for(int k=minr;k<=maxr && count<r*c;k++){
-             v.push_back(matrix[k][maxc]);
-             count++;
-        }
-        maxc--;
-         for(int l=maxc;l>=minc && count<r*c;l--){
-             v.push_back(matrix[maxr][l]);
-             count++;
-        }
-        maxr--;
-         for(int m=maxr;m>=minr && count<r*c;m--){
-             v.push_back(matrix[m][minc]);
-             count++;
-            
-        }
-         minc++;
+           int left=0,top=0,bottom=r-1,right=c-1;
+          // int count =0;
+   while((left<=right) && (top<=bottom)){
+    
+    for(int i=left;i<=right && count<num;i++){
+        v.push_back(matrix[left][i]);
+        count++;
+    }
+    top++;
+     for(int i=top;i<=bottom && count<num;i++){
+        v.push_back(matrix[i][right]);
+        count++;
+    }
+    right--;
+     for(int i=right;i>=left && count<num;i--){
+        v.push_back(matrix[bottom][i]);
+        count++;
+    }
+    bottom--;
+     for(int i=bottom;i>=top && count<num;i--){
+        v.push_back(matrix[i][left]);
+        count++;
+    }
+    left++;
 }
         return v;
     }
