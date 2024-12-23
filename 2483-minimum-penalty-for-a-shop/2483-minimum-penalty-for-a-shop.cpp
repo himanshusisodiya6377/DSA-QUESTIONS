57d5a0997a7a30b491dc763123db7982245a6991
Himanshu Sisodiya <hsisodia988766@gmail.com>
2024-12-23 17:26:00 +0530

    Time: 1733 ms (5.04%), Space: 25.8 MB (5.28%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n=customers.size();
-        int pre[n+1];
-        int suff[n+1];
-        int ans[n+1];
-        for(int i=0;i<n;i++){ //prefix for N
-            pre[0]=0;
-             if(customers[i]=='N') pre[i+1]=pre[i]+1;
-             else pre[i+1]=pre[i];
+         int n=customers.size();
+        vector<int>pre(n+1,0);
+        vector<int>suff(n+1,0);
+        vector<int>ans(n+1,0);
+       //for N in the string
+       for(int i=1;i<n+1;i++){
+        if(customers[i-1]=='N') suff[i]+=(suff[i-1]+1);
+        else suff[i]+=(suff[i-1]+0);
+        // cout<<suff[i-1]<<" ";
+       }
+       cout<<endl;
+       //for Y in the string
+       for(int i=n-1;i>=0;i--){
+        if(customers[i]=='Y') pre[i]+=(pre[i+1]+1);
+        else pre[i]+=(pre[i+1]+0);
+       // cout<<pre[i]<<" ";
+       }
+       cout<<endl;
+       for(int i=0;i<n+1;i++){
+          ans[i]=pre[i]+suff[i];
+          cout<<ans[i]<<" ";
+       }
+       
+       int min=INT_MAX;
+       int idx=-1;
+       for(int i=0;i<n+1;i++){
+        if(min>ans[i]){
+            min=ans[i];
+            idx=i;
         }
-        for(int i=n-1;i>=0;i--){
-            suff[n]=0;
-            if(customers[i]=='Y') suff[i]=suff[i+1]+1;
-            else suff[i]=suff[i+1];
-        }
-        for(int i=0;i<=n;i++){
-            ans[i]=pre[i]+suff[i];
-        }
-        int min=INT_MAX;
-         int idx=-1;
-        for(int i=0;i<=n;i++){
-            if(ans[i]<min){
-              min=ans[i];
-              idx=i;
-            }
-        }
-        return idx;
+       }
+       return idx;
     }
 };
