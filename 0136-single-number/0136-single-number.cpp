59c0ea031b2bd7871ef9f273bb113b3965acbc60
Himanshu Sisodiya <hsisodia988766@gmail.com>
2024-12-23 11:56:04 +0530

    Time: 0 ms (100%), Space: 20.6 MB (38.23%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,14 +3,20 @@ public:
     int singleNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        int i=0;
-        while(i<n){
-          if(i==n-1) return nums[i];
-           else if(nums[i]==nums[i+1]){
-                i+=2;
-            }
-            else return nums[i];
+        // int i=0;
+        // while(i<n){
+        //   if(i==n-1) return nums[i];
+        //    else if(nums[i]==nums[i+1]){
+        //         i+=2;
+        //     }
+        //     else return nums[i];
+        // }
+        // return -1;
+        //optimum approach through xor operation
+        int ans=0;
+        for(int i=0;i<n;i++){
+         ans^=nums[i];
         }
-        return -1;
+        return ans;
     }
 };
