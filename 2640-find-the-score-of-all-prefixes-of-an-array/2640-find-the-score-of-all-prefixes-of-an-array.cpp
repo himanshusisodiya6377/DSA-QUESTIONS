b8e21aa206d294183f085824ca9433f1176a6ea2
Himanshu Sisodiya <hsisodia988766@gmail.com>
2024-12-24 21:29:25 +0530

    Time: 5 ms (20.38%), Space: 58.9 MB (64.22%) - LeetHub

--- 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.cpp
+++ 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     vector<long long> findPrefixScore(vector<int>& nums) {
+        // int n=nums.size();
+        // int max=nums[0];
+        // vector<long long>v;
+        // long long int pre[n];
+        // pre[0]=2*nums[0];
+        // v.push_back(pre[0]);
+        // for(int i=1;i<n;i++){
+        //        if(max<nums[i]) max=nums[i];
+        //     pre[i]=nums[i]+max+pre[i-1];
+        //      v.push_back(pre[i]);
+        // }
+        // return v;
         int n=nums.size();
-        int max=nums[0];
-        vector<long long>v;
-        long long int pre[n];
-        pre[0]=2*nums[0];
-        v.push_back(pre[0]);
+        vector<long long>v(n,0);
+        int maxi=nums[0];
+        v[0]=maxi+nums[0];
         for(int i=1;i<n;i++){
-               if(max<nums[i]) max=nums[i];
-            pre[i]=nums[i]+max+pre[i-1];
-             v.push_back(pre[i]);
+            maxi=max(maxi,nums[i]);
+            v[i]+=(v[i-1]+maxi+nums[i]);
         }
-        return v;
+       return v;
     }
 };
