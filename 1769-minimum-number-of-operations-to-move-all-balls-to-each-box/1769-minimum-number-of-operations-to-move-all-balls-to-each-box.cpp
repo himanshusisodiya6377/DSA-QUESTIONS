a343439563ed0fc2b2c20da39b4e3164f1b84298
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-01-06 09:14:57 +0530

    Time: 0 ms (100%), Space: 12.1 MB (50.97%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -3,22 +3,30 @@ public:
     vector<int> minOperations(string boxes) {
         int n=boxes.size();
         vector<int>pre(n,0);
-        vector<int>suff(n,0);
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(boxes[j]=='1'){
-                    pre[i]+=abs(j-i);
-                }
-            }
+        
+        // vector<int>suff(n,0);
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(boxes[j]=='1'){
+        //             pre[i]+=abs(j-i);
+        //         }
+        //     }
+        // }
+        //optimise
+        int one=boxes[0]=='1';
+        
+        for(int i=1;i<n;i++){//prefix sum calculation
+             pre[i]=pre[i-1]+one;
+             if(boxes[i]=='1') one++;
         }
-        for(int i=n-1;i>=1;i--){
-            for(int j=i-1;j>=0;j--){
-                if(boxes[j]=='1'){
-                    suff[i]+=abs(j-i);
-                }
-            }
+        one=0;
+        int prev=0;
+        for(int i=n-1;i>=0;i--){//suffix sum add in prefix 
+            pre[i]=pre[i]+prev;
+            if(boxes[i]=='1') one++;
+            prev+=one;
         }
-         for(int i=0;i<n;i++) pre[i]+=suff[i];
+        //  for(int i=0;i<n;i++) pre[i]+=suff[i];
          return pre;
     }
 };
