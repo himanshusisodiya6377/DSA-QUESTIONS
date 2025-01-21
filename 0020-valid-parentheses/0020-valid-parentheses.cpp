d65d2288d39789205f1973c8c538587fb713ad82
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-01-21 07:20:21 +0530

    Time: 4 ms (2.01%), Space: 10 MB (7.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
+        //T.C=O(n);
         char ch;
         for(int i=0;i<s.length();i++){
+            //iterating every element of string
             if(st.size()!=0) ch=st.top();
             if(s[i]=='(') st.push('(');
             else if(s[i]=='{') st.push('{');
             else if(s[i]=='[') st.push('[');
+            //get ch than compare for pop 
             else if(ch=='(' && s[i]==')'){
             st.pop();
-            ch=' ';
+            //important to enpty other wise runtime it will try to access empty stack
+            ch=' ';//when stack is going to empty but any if else condition
+            //going to compare with previous old ch so thats why need to empty it
             } 
             else if(ch=='{' && s[i]=='}'){
                  st.pop();
