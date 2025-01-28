264ff2e6b766d58a880eb84c445d42f0dd89a734
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-01-28 14:14:44 +0530

    Time: 19 ms (34.33%), Space: 88.6 MB (78.7%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -5,10 +5,8 @@ public:
          vector<int>v(n,0);
     stack<int>idx;
     //TC:-O(n) SC:-O(n) because of use of stack at worst case there can be all elements in stack
-    //push always first and -1 in v as no previous greater
-    //than move with for loop and pop all element if its smaller 
-    //than store in v[i] and push in stack
-    //simple pop v[i] push traverse forward
+    //calculate the number of pop as it is equal to the persons he can watch
+    //if size not zero means num+1 else num  easy question
     idx.push(n-1);
     for(int i=n-2;i>=0;i--){
         int num=0;
