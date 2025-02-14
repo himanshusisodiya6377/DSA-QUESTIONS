5906320974da38d59764ba96805107c28d72157a
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-02-14 10:23:08 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.62%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
         int n=nums.size();
         // int i=0;
         // while(i<n){
