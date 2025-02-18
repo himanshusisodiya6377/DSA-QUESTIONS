31bde6e9dab23abf8bc9c5604cf3b7dffdd09bb1
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-02-18 21:31:32 +0530

    Time: 42 ms (15.55%), Space: 73.2 MB (37.87%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        unordered_map<int,int>ms;
-        unordered_map<int,int>mf;
-        int k=trust.size();
-        if(k==0){
-            if(n==1) return n;
-            else return -1;
-        }
-        for(int i=0;i<k;i++){
-            ms[trust[i][1]]++;
-            mf[trust[i][0]]++;
-        }
-      int check=0;
-        for(auto p: ms){
-            if(p.second==n-1) check=p.first;
-        }
+        //TC-O(n) //SC-O(n)
+    //     unordered_map<int,int>ms;
+    //     unordered_map<int,int>mf;
+    //     int k=trust.size();
+    //     if(k==0){
+    //         if(n==1) return n;
+    //         else return -1;
+    //     }
+    //     for(int i=0;i<k;i++){
+    //         ms[trust[i][1]]++;
+    //         mf[trust[i][0]]++;
+    //     }
+    //   int check=0;
+    //     for(auto p: ms){
+    //         if(p.second==n-1) check=p.first;
+    //     }
        
-            if(mf.find(check)==mf.end() && check!=0){
-               return check;
-            }
+    //         if(mf.find(check)==mf.end() && check!=0){
+    //            return check;
+    //         }
         
-       return -1;
+    //    return -1;
+    //TC-O(n) //SC-O(1)
+      vector<int> Trusted(n+ 1, 0);
+        for(auto person : trust){
+            Trusted[person[0]]--;
+            Trusted[person[1]]++;
+        }
+        for(int i = 1;i <= n;i++){
+            if(Trusted[i] == n - 1)
+                return i;
+        }
+        return -1;
     }
 };
