6e2219d745cf5f23246cb6d3d87f412f5aef104e
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-02-21 22:35:44 +0530

    Time: 0 ms (100%), Space: 20.6 MB (38.09%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -2,13 +2,32 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int n=nums.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0) ans.push_back(nums[i]);
+        // vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]%2==0) ans.push_back(nums[i]);
+        // }
+        //  for(int i=0;i<n;i++){
+        //     if(nums[i]%2!=0) ans.push_back(nums[i]);
+        // }
+        // return ans;
+        int i=0,j=n-1;
+        while(i<j){
+            if(nums[i]%2!=0 && nums[j]%2==0){
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+            else if(nums[i]%2==0 && nums[j]%2!=0){
+                j--;
+                i++;
+            }
+            else if(nums[i]%2==0 && nums[j]%2==0){
+                i++;
+            }
+            else{
+                j--;
+            }
         }
-         for(int i=0;i<n;i++){
-            if(nums[i]%2!=0) ans.push_back(nums[i]);
-        }
-        return ans;
+        return nums;
     }
 };
