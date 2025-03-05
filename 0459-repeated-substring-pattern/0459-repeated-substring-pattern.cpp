ae89d9625e7dc3dab7187a90da363c149a4ba948
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-05 19:27:42 +0530

    Time: 24 ms (45.02%), Space: 47.7 MB (39.54%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        int n=s.length();
-        // if(n%2!=0) return false;
-        // int m=n/2;
-        // string s1=s.substr(0,m);
-        // string s2=s.substr(m,m);
-        // if(s1==s2) return true;
-        // else return false;
-        string copy=s;
-        int i=0;
-        while(i<n-1){
-            char ch=s[0];
-            s.erase(0,1);
-            s.push_back(ch);
-            if(s==copy) return true;
-            i++;
-        } 
+        int n = s.length();
+        //tc-O(n^2)
+        // string copy = s;
+        // int i = 0;
+        // while (i < n - 1) {
+        //     char ch = s[0];
+        //     s.erase(0, 1);
+        //     s.push_back(ch);
+        //     if (s == copy)
+        //         return true;
+        //     i++;
+        // }
+        // return false;
+
+        //optimised approach
+        for(int i=n/2;i>0;i--){
+            if(n%i==0){
+                int times=n/i;
+                string ans="";
+                string sun=s.substr(0,i);
+                while(times--){
+                     ans+=sun;
+                }
+                if(ans==s) return true;
+            }
+        }
         return false;
     }
 };
