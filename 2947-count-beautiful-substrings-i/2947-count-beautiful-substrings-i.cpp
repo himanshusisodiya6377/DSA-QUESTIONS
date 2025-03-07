05a282dca8ed66aa25ac52c75e88985348a062cd
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-07 09:47:50 +0530

    Time: 35 ms (62.53%), Space: 9.8 MB (95.04%) - LeetHub

--- 2947-count-beautiful-substrings-i/2947-count-beautiful-substrings-i.cpp
+++ 2947-count-beautiful-substrings-i/2947-count-beautiful-substrings-i.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int beautifulSubstrings(string s, int k) {
-        vector<int>prevow(s.length(),0);
-        vector<int>precon(s.length(),0);
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
-                prevow[i]++;
-            }
-            else precon[i]++;
-        }
+        // vector<int>prevow(s.length(),0);
+        // vector<int>precon(s.length(),0);
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+        //         prevow[i]++;
+        //     }
+        //     else precon[i]++;
+        // }
         int count=0;
         for(int i=0;i<s.length();i++){
             int lengthvow=0;
             int lengthcon=0;
             for(int j=i;j<s.length();j++){
-                if(prevow[j]!=0) lengthvow+=prevow[j];
-                else lengthcon+=precon[j];
+                if(s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o' || s[j]=='u') lengthvow++;
+                else lengthcon++;
                 if(lengthvow==lengthcon){
                     int prod=lengthvow*lengthcon;
                     if(prod%k==0) count++;
