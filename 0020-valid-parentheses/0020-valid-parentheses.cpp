8e9e57676201acf9eb0a8e85bf465cc2bc8a33e2
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-08 23:21:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (83.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -6,24 +6,19 @@ public:
         char ch;
         for(int i=0;i<s.length();i++){
             //iterating every element of string
-            if(st.size()!=0) ch=st.top();
-            if(s[i]=='(') st.push('(');
-            else if(s[i]=='{') st.push('{');
-            else if(s[i]=='[') st.push('[');
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{') st.push(s[i]);
             //get ch than compare for pop 
-            else if(ch=='(' && s[i]==')'){
+            else if(st.size()>0 && st.top()=='(' && s[i]==')'){
             st.pop();
             //important to enpty other wise runtime it will try to access empty stack
-            ch=' ';//when stack is going to empty but any if else condition
+            //when stack is going to empty but any if else condition
             //going to compare with previous old ch so thats why need to empty it
             } 
-            else if(ch=='{' && s[i]=='}'){
+            else if(st.size()>0 && st.top()=='{' && s[i]=='}'){
                  st.pop();
-                  ch=' ';
             } 
-            else if(ch=='[' && s[i]==']') {
+            else if(st.size()>0 && st.top()=='[' && s[i]==']') {
             st.pop();
-            ch=' ';
             }
             else return false;
         }
