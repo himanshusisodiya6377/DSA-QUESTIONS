511bdb256eebdc6d12d5153c2bca30dc5e27bb40
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-09 19:02:56 +0530

    Time: 1 ms (27.47%), Space: 8.5 MB (23.1%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -1,51 +1,52 @@
 class Solution {
 public:
     string smallestNumber(string pattern) {
-        int k=pattern.length();
-        string ans="";
-        stack<char>st;
-        stack<char>temp;
-        st.push(1);
-        int count=2;
-        int n=0;
-        for(int i=0;i<k;i++){
-            if(pattern[i]=='I'){
-                while(temp.size()!=0){
-                    int m=temp.top();
+        int k = pattern.length();
+        string ans = "";
+        // stack<char> st;
+        stack<char> temp;
+       ans+=to_string(1);
+        int count = 2;
+        int n = 0;
+        for (int i = 0; i < k; i++) {
+            if (pattern[i] == 'I') {
+                while (temp.size() != 0) {
+                    int m = temp.top();
                     temp.pop();
-                    st.push(m);
+                   ans+=to_string(m);
                 }
-                if(n!=0){
-                    st.push(n);
-                    n=0;
+                if (n != 0) {
+                    // st.push(n);
+                    ans+=to_string(n);
+                    n = 0;
                 }
-                st.push(count);
+               ans+=to_string(count);
                 count++;
-            }
-            else{
-                if(n==0){
-                    n=st.top();
-                st.pop();
+            } else {
+                if (n == 0) {
+                    n = ans.back()-48;
+                    ans.pop_back();
                 }
                 temp.push(count);
                 count++;
             }
         }
-         while(temp.size()!=0){
-                    int m=temp.top();
-                    temp.pop();
-                    st.push(m);
-                }
-                if(n!=0){
-                    st.push(n);
-                    n=0;
-                }
-                while(st.size()!=0){
-                    int n=st.top();
-                    ans+=to_string(n);
-                    st.pop();
-                }
-                reverse(ans.begin(),ans.end());
+        while (temp.size() != 0) {
+            int m = temp.top();
+            temp.pop();
+            // st.push(m);
+            ans+=to_string(m);
+        }
+        if (n != 0) {
+           ans+=to_string(n);
+            n = 0;
+        }
+        // while (st.size() != 0) {
+        //     int n = st.top();
+        //     ans += to_string(n);
+        //     st.pop();
+        // }
+        // reverse(ans.begin(), ans.end());
         return ans;
     }
 };
