f5ae716d5accdc65370d2934a6676c7a791f4f36
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-09 19:40:03 +0530

    Time: 90 ms (8.79%), Space: 108 MB (40.59%) - LeetHub

--- 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
@@ -1,62 +1,25 @@
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
-        int n=colors.size();
-        int count=0;
-        int var;
-        // bool flag=false;
-        for (int i = 0; i < n; i++) {
-    bool isAlternating = true;
-
-    for (int j = 0; j < k - 1; j++) {  // Check `k` elements starting from `i`
-        int currIndex = (i + j) % n;  // Ensure circular behavior
-        int nextIndex = (i + j + 1) % n;
-        
-        if (colors[currIndex] == colors[nextIndex]) {
-            isAlternating = false;
-            break;
-        }
-    }
-
-    if (isAlternating){
-        var=i;
-        break;
-    }
-}
-        // for(int i=0;i<n;i++){
-        //     int r=(i+k-1);
-        //   for(int j=i;j<r-2;j++){
-        //      int ch=j%n;
-        //     int m=(j+1)%n;
-           
-        //     if(colors[ch]==colors[m]) {
-        //         flag=false;
-        //         break;
-        //     }
-        //     else {
-        //         flag=true;
-        //     }
-        //   }
-        //   if(flag==true){
-        //        var=i;
-        //         break;
-        //   } 
-        // }
-        for(int i=var;i<n;i++){
-            int j=(i+k-1)%n;
-            int p=(i+1)%n;
-            int m;
-            if(j==0){
-                m=n-1;
-            }
-            else m=j-1;
-            if(k%2==0){
-                if(colors[i]!=colors[j] && colors[i]!=colors[p] && colors[j]!=colors[m]) count++;
-            }
-            else{
-                 if(colors[i]==colors[j] && colors[i]!=colors[p] && colors[j]!=colors[m]) count++;
-            }
-        }
-      return count;
+       int n=colors.size();
+       for(int i=0;i<k-1;i++){
+        colors.push_back(colors[i]);//for circular array
+       }
+       int count=0;
+       int j=1;
+       int i=0;
+       while(j<(n+k-1)){
+          if(colors[j]==colors[j-1]){
+            i=j;
+            j++;
+            continue;
+          }
+           if(j-i+1==k) {
+            count++;
+            i++;
+           }
+          j++;
+       }
+       return count;
     }
 };
