c334a4dba8713935763f7b46f010ec0446944763
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-09 19:28:02 +0530

    Time: 12 ms (63.91%), Space: 32 MB (70.86%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int minimumLength(string s) {
         int n=s.length();
-        unordered_map<char,int>mp;
+        vector<int>freq(26,0);
         for(int i=0;i<n;i++){
-            mp[s[i]]++;
+            freq[s[i]-'a']++;
         }
         int ans=0;
-        for(auto p : mp){
-            int m=(p.second);
-            if(m%2==0) ans+=2;
-            else ans+=1;
+        for(int i=0;i<26;i++){
+            if(freq[i]%2==0 && freq[i]!=0) ans+=2;
+            else if(freq[i]%2!=0) ans+=1;
         }
         return ans;
     }
