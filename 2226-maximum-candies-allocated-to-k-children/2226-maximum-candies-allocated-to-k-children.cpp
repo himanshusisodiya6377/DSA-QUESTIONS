d3285a8dd2e7442f1cd5dcc9c2bfd91e49de0636
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-14 09:28:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        int n = candies.size();
+        int sum = 0;
+        int maxi = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            sum += candies[i];
+            if (maxi < candies[i])
+                maxi = candies[i];
+        }
+        int result = 0;
+        if (sum < k)
+            return 0;
+        else {
+            int lo = 1, hi = maxi;
+            while (lo <= hi) {
+                int mid = (lo + hi) / 2;
+                long long count = 0;
+                for (int c : candies) {
+                    count += c / mid;
+                }
+                if (count >= k) {
+                    result = mid;
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return result;
+    }
+};
