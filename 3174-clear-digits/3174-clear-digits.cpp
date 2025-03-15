32f398e12a4aa9863ffafcb511ffbcf484e4260a
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-15 20:08:40 +0530

    Time: 0 ms (100%), Space: 8.5 MB (60.51%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        int n=s.length();
-        string ans="";
-        for(int i=0;i<n;i++){
-            if(s[i]>=48 && s[i]<=57){
-                if(ans.size()!=0){
+        int n = s.length();
+        string ans = "";
+        for (int i = 0; i < n; i++) {
+            if (s[i] >= 48 && s[i] <= 57) {
+                if (ans.size() != 0) {
                     ans.pop_back();
                 }
-            }
-            else ans.push_back(s[i]);
+            } else
+                ans.push_back(s[i]);
         }
         return ans;
     }
