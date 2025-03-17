cfcbf97975a7d14a322ca11dd6a22e9ab2c2eec1
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-17 10:15:01 +0530

    Time: 0 ms (100%), Space: 7.9 MB (36.38%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    bool check(int mid,int n){
-          long long int multi=(long long )mid*mid-mid;
-          long long int x=mid+multi;
-          if(x<=n) return true;
-          else return false;
+    bool check(int mid, int n) {
+        long long int multi = (long long)mid * mid - mid;
+        long long int x = mid + multi;
+        if (x <= n)
+            return true;
+        else
+            return false;
     }
     int bulbSwitch(int n) {
-        if(n==1 || n==2) return 1;
-        int lo=1,hi=n;
-        int mid;
-        while(lo<=hi){
-           mid=(lo+hi)/2;
-            if(check(mid,n)) lo=mid+1;
-            else hi=mid-1;
-        }
-        return hi;
+        return sqrt(n);
+        // if (n == 1 || n == 2)
+        //     return 1;
+        // int lo = 1, hi = n;
+        // int mid;
+        // while (lo <= hi) {
+        //     mid = (lo + hi) / 2;
+        //     if (check(mid, n))
+        //         lo = mid + 1;
+        //     else
+        //         hi = mid - 1;
+        // }
+        // return hi;
     }
 };
