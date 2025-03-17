1b4644e5c9e651735bbe51d9a0952a8aed9dc0f0
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-17 09:23:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (77.99%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -9,9 +9,12 @@ public:
         return ans;
     }
     int addDigits(int num) {
-        while(num>9){
-            num=calculate(num);
-        }
-        return num;
+        // while(num>9){
+        //     num=calculate(num);
+        // }
+        // return num;
+        if(num==0) return 0;
+        if(num%9==0) return 9;
+        return num%9;
     }
 };
