f3dbab8fa5d181d6ccdfaab20a2e19e488aa8627
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-19 09:37:34 +0530

    Time: 103 ms (14.03%), Space: 18.3 MB (80.63%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int row=lists.size();
-        if(row==0) return NULL;
-        int i=1;
-        ListNode*c=new ListNode(-100);
-        ListNode*temp=c;
-        ListNode*temp1=lists[lists.size()-1];
-        lists.pop_back();
-         temp->next=temp1;
-    while(i<row){
-            ListNode*temp2=lists[lists.size()-1];
-            i++;
-    while(temp1!=NULL && temp2!=NULL){
-        if(temp1->val <= temp2->val){
+ ListNode* merge(ListNode* list1, ListNode* list2) {
+   ListNode*c=new ListNode(-100);
+   ListNode*temp=c;
+   ListNode*temp1=list1;
+   ListNode*temp2=list2;
+   while(temp1!=NULL && temp2!=NULL){
+    if(temp1->val < temp2->val){
+        temp->next=temp1;
         temp1=temp1->next;
         temp=temp->next;
-           }
-          else{
+    }
+    else{
         temp->next=temp2;
         temp2=temp2->next;
         temp=temp->next;
-        temp->next=temp1;
     }
-         if(temp1==NULL) temp->next=temp2;
-   else temp->next=temp1;
-
-            }
-         
-    lists.pop_back();
-    temp=c;
-    temp1=temp->next;
    
-       }
-     return c->next;
+   }
+   if(temp1==NULL) temp->next=temp2;
+   else temp->next=temp1;
+ return c->next;
     }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return NULL;
+        if(lists.size()==1) return lists[0];
+        while(lists.size()>1){
+          ListNode*a=lists[lists.size()-1];
+        lists.pop_back();
+        ListNode*b=lists[lists.size()-1];
+        lists.pop_back();
+        ListNode*c=merge(a,b);
+        lists.push_back(c);
+        }
+       return lists[0];
+    }
+    
 };
