557505e7ef97706e93ee5e0de997718442c6922e
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-20 21:42:10 +0530

    Time: 2 ms (38.04%), Space: 14 MB (20.95%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_set<int>st;
-        int n=nums.size();
-        long long sum=0;
-        for(auto i : nums){
-            st.insert(i);
-            sum+=i;
+        // unordered_set<int>st;
+        // int n=nums.size();
+        // long long sum=0;
+        // for(auto i : nums){
+        //     st.insert(i);
+        //     sum+=i;
+        // }
+        // long long setsum=0;
+        // for(auto p : st){
+        //     setsum+=p;
+        // }
+        // sum-=setsum;
+        // sum/=2;
+        // setsum-=sum;
+        // return setsum;
+        unordered_map<int,int>mp;
+        for(auto i : nums) mp[i]++;
+        for(auto p : mp){
+            if(p.second==1) return p.first;
         }
-        long long setsum=0;
-        for(auto p : st){
-            setsum+=p;
-        }
-        sum-=setsum;
-        sum/=2;
-        setsum-=sum;
-        return setsum;
+        return -1;
     }
 };
