5f44b538753d8487050a0f6cc6d4121b308ad132
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-20 07:05:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,135 +1,24 @@
 class Solution {
 public:
-    int upparbound(vector<int>arr,int k){
-         int n=arr.size();
-       int first=-1;
-       int lo=0,hi=n-1;
-       while(lo<=hi){
-           int mid=(lo+hi)/2;
-           if(arr[mid]<=k){
-               first=mid;
-               lo=mid+1;
-           }
-           else hi=mid-1;
-       }
-       return first;
-    }
-     int lowerbound(vector<int>arr,int k){
-         int n=arr.size();
-       int last=-1;
-       int lo=0,hi=n-1;
-       while(lo<=hi){
-          int mid=(lo+hi)/2;
-           if(arr[mid]>=k){
-               last=mid;
-               hi=mid-1;
-           }
-           else lo=mid+1;
-       }
-       return last;
-    }
-    vector<int> searchRange(vector<int>& arr, int k) {
-        //PLAIN BINARY SEARCH APPROACH
-              int n=arr.size();
-              vector<int>v(2,-1);
-              //to getting first element simply use lower bound
-       int first=-1;
-       int lo=0,hi=n-1;
-       while(lo<=hi){
-           int mid=(lo+hi)/2;
-           if(arr[mid]==k){
-               first=mid;
-               hi=mid-1;
-           }
-           else if(arr[mid]<k) lo=mid+1;
-           else hi=mid-1;
-       }
-       if(first>=0 && arr[first]==k) v[0]=first;
-         int last=-1;
-         lo=0,hi=n-1;
-       while(lo<=hi){
-          int mid=(lo+hi)/2;
-           if(arr[mid]==k){
-               last=mid;
-               lo=mid+1;
-           }
-           else if(arr[mid]<k) lo=mid+1;
-           else hi=mid-1;
-       }
-      if(last>=0 && arr[last]==k) v[1]=last;
-       return v;
-        //LOWER AND UPPAR BOUND APPROACH
-        // if(nums.size()==0) return {-1,-1};
-        //      int x=lowerbound(nums,target);
-        //      int y=upparbound(nums,target);
-        //      if(x>=0 && nums[x]==target) return{x,y};
-        //      else return {-1,-1};
-//BAKWASS APPROACH (1ST MINE APPROACH!!)
-    //   int n=nums.size();
-    //   vector<int>v={-1,-1};//decalaration of vector
-    //   if(n==0) return v;
-    //   if(n==1){
-    //     if(nums[0]==target){
-    //         v[0]=0;
-    //         v[1]=0;
-    //         return v;
-    //     }
-    //     else {
-    //         return v;
-    //     }
-    //   }
-    //   int lo=0,hi=n-1;
-    //   while(lo<=hi){
-    //     int mid=(hi+lo)/2;
-    //     if(nums[mid]<target) lo=mid+1;
-    //     else if(nums[mid]>target) hi=mid-1;
-    //     else{
-    //         if(mid==0){//if  mid at zero so target to get last occurance
-    //             v[0]=mid;
-    //             int k=mid;
-    //             int j=1;
-    //              while(j<n && nums[mid+j]==target) {
-    //                 k++;
-    //                 j++;
-    //         }
-    //         v[1]=k;
-    //         return v;
-    //         }
-    //         else if(mid==n-1){ //if mid at n-1 target to get first occurance
-    //             int j=1;
-    //             int k=mid;
-    //             while(k>0 && nums[n-1-j]==target) {
-    //                 k--;
-    //                 j++;
-    //         }
-    //         v[0]=k;
-    //         v[1]=mid;
-    //         return v;
-    //     }
-    //     else{  //if mid in between
-    //         int pre=mid;//for first occurance
-    //         int pro=mid;//for  last occurance
-    //         int j=0;
-    //         if(nums[mid-1]==target){
-    //             while(pre>0 && nums[mid-1-j]==target){
-    //                 pre--;
-    //                 j++;
-    //             }
-    //         }
-    //         j=0;
-    //         if(nums[mid+1]==target){
-    //             while(pro<n-1 && nums[mid+1+j]==target){
-    //                 j++;
-    //                 pro++;
-    //             }
-    //         }
-    //         v[0]=pre;
-    //         v[1]=pro;
-    //         return v;
-    //     }
-    //   }
-    
-    // }
-    //   return v;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        if(n==0) return {-1,-1};
+        int lo=0,hi=n-1;
+        int first=-1;
+        while(lo<=hi){//lower bound
+            int mid=(lo+hi)/2;
+            if(nums[mid]<target) lo=mid+1;
+            else if(nums[mid]>=target) hi=mid-1;
+        }
+        if(nums[lo]==target) first=lo;
+        lo=0,hi=n-1;
+        int last=-1;
+        while(lo<=hi){//uppar bound
+            int mid=(lo+hi)/2;
+            if(nums[mid]>target) hi=mid-1;
+            else if(nums[mid]<=target) lo=mid+1;
+        }
+        if(nums[hi]==target) last=hi;
+        return {first,last};
     }
 };
