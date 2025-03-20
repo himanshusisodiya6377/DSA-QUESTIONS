43a2a6f1e9a5a353bbc67348a865969b14023284
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-20 21:51:24 +0530

    Time: 1 ms (44.36%), Space: 13.4 MB (66.11%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         // unordered_set<int>st;
-        // int n=nums.size();
+        int n=nums.size();
         // long long sum=0;
         // for(auto i : nums){
         //     st.insert(i);
@@ -16,11 +16,28 @@ public:
         // sum/=2;
         // setsum-=sum;
         // return setsum;
-        unordered_map<int,int>mp;
-        for(auto i : nums) mp[i]++;
-        for(auto p : mp){
-            if(p.second==1) return p.first;
-        }
-        return -1;
+
+
+        // unordered_map<int,int>mp;
+        // for(auto i : nums) mp[i]++;
+        // for(auto p : mp){
+        //     if(p.second==1) return p.first;
+        // }
+        // return -1;
+
+          sort(begin(nums),end(nums));
+          if(n==1) return nums[0];
+          if(nums[0]!=nums[1]) return nums[0];
+          int i=1;
+          while(i<n-1){
+            if(nums[i]==nums[i+1] && nums[i]==nums[i-1]){
+                i+=3;
+            }
+            else if(nums[i]==nums[i+1] && nums[i]!=nums[i-1]) return nums[i-1];
+            else if(nums[i]!=nums[i+1] && nums[i]==nums[i-1]) return nums[i+1];
+            else return nums[i];
+          }
+          if(nums[n-1]!=nums[n-2]) return nums[n-1]; 
+          return -1;
     }
 };
