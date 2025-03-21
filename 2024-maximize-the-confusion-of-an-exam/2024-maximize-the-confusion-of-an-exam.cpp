b33fbdbe515050d5491aa6019249abb772064864
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-21 09:33:31 +0530

    Time: 16 ms (78.7%), Space: 12.6 MB (52.37%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-       int n=answerKey.length();
-       int i=0,j=1;
-       int maxi=INT_MIN;
-       int countt=0;
-       int countf=0;
-       if(answerKey[0]=='T') countt++;
-       else countf++;
-         if(answerKey[1]=='T') countt++;
-       else countf++;
-       while(j<n){
-         int mini=min(countf,countt);
-         if(mini<=k){
-            j++;
-          if(answerKey[j]=='T') countt++;
-          else countf++;
-         }
-         else{
-            int diff=j-i;
-            maxi=max(maxi,diff);
-            i++;
-          if(answerKey[i-1]=='T') countt--;
-          else countf--;
-         }
-       }
-       maxi=max(maxi,j-i);
-       return maxi;
+        int n = answerKey.length();
+        //basic sliding window
+        //concept of min between both true and false
+        int i = 0, j = 1;
+        int maxi = INT_MIN;
+        int countt = 0;
+        int countf = 0;
+        unordered_map<char,int>mp;
+        mp[answerKey[0]]++;
+        mp[answerKey[1]]++;
+        // if (answerKey[0] == 'T')
+        //     countt++;
+        // else
+        //     countf++;
+        // if (answerKey[1] == 'T')
+        //     countt++;
+        // else
+        //     countf++;
+        while (j < n) {
+            int mini = min(mp['T'],mp['F']);
+            if (mini <= k) {
+                j++;
+                // if (answerKey[j] == 'T')
+                //     countt++;
+                // else
+                //     countf++;
+                mp[answerKey[j]]++;
+            } else {
+                int diff = j - i;
+                maxi = max(maxi, diff);
+                i++;
+                // if (answerKey[i - 1] == 'T')
+                //     countt--;
+                // else
+                //     countf--;
+                mp[answerKey[i-1]]--;
+            }
+        }
+        maxi = max(maxi, j - i);
+        return maxi;
     }
 };
