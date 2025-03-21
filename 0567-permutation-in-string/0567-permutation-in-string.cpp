fbd65d691643bf27c270561613cfb07f8f83dd3a
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-21 20:00:30 +0530

    Time: 1 ms (74.11%), Space: 10.1 MB (34.65%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -5,6 +5,7 @@ public:
         vector<int>sec(26,0);
         int n=s1.length();
         int m=s2.length();
+        if(n>m) return false;
         for(int i=0;i<n;i++){
             fir[s1[i]-'a']++;
         }
@@ -12,13 +13,14 @@ public:
         for(int i=0;i<k;i++){
             sec[s2[i]-'a']++;
         }
+        if(sec==fir) return true;
         int i=0,j=k-1;
         while(j<m-1){
-           if(sec==fir) return true;
            sec[s2[i]-'a']--;
            i++;
            j++;
            sec[s2[j]-'a']++;
+           if(sec==fir) return true;
         }
         return false;
     }
