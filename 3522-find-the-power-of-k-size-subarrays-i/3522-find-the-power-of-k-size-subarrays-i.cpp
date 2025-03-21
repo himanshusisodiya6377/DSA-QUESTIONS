348d2eb86f26c92bcc8d0e7d4cae25cdfb3561cd
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-21 18:51:00 +0530

    Time: 0 ms (100%), Space: 34.1 MB (98.15%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
-        int n=nums.size();
-        if(k==1) return nums;
-        vector<int>v(n-k+1,-1);
-        int idx=-1;
-        for(int i=0;i<k-1;i++){
-            if((nums[i]+1)!=nums[i+1]) idx=i;
+        int n = nums.size();
+        if (k == 1)
+            return nums;
+        vector<int> v(n - k + 1, -1);
+        int idx = -1;
+        for (int i = 0; i < k - 1; i++) {
+            if ((nums[i] + 1) != nums[i + 1])
+                idx = i;
         }
-        int m=0;
-        if(idx==-1) v[m]=nums[k-1];
+        int m = 0;
+        if (idx == -1)
+            v[m] = nums[k - 1];
         m++;
-        int i=1,j=k;
-        while(j<n){
-            if(idx>=i && idx<=j){
-                if(nums[j]!=(nums[j-1]+1)) idx=j-1;
+        int i = 1, j = k;
+        while (j < n) {
+            if (idx >= i && idx <= j) {
+                if (nums[j] != (nums[j - 1] + 1))
+                    idx = j - 1;
                 i++;
                 j++;
                 m++;
-              
+
                 continue;
             }
-            if(nums[j]==(nums[j-1]+1)){
-                v[m]=nums[j];
+            if (nums[j] == (nums[j - 1] + 1)) {
+                v[m] = nums[j];
                 m++;
                 j++;
                 i++;
-            } 
-            else{
-                idx=j-1;
+            } else {
+                idx = j - 1;
                 m++;
                 i++;
                 j++;
