ecff60b8a02f0e8da4a95dbf5fc6ef5583ef1a39
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-22 11:21:25 +0530

    Time: 0 ms (100%), Space: 11.2 MB (35.24%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    void check(vector<int>&ans,int n){
-        int cnt=0;
-        if(n==0) {
-            ans.push_back(0);
-            return;
-        }
-        while(n>1){
-            if(n%2==1) cnt++;
-            n/=2;
-        }
-        cnt++;
-        ans.push_back(cnt);
-    }
+    // void check(vector<int>&ans,int n){
+    //     int cnt=0;
+    //     if(n==0) {
+    //         ans.push_back(0);
+    //         return;
+    //     }
+    //     while(n>1){
+    //         if(n%2==1) cnt++;
+    //         n/=2;
+    //     }
+    //     cnt++;
+    //     ans.push_back(cnt);
+    // }
     vector<int> countBits(int n) {
         vector<int>ans;
         for(int i=0;i<=n;i++){
-            check(ans,i);
+            // check(ans,i);
+            int x=__builtin_popcount(i);
+            ans.push_back(x);
         }
         return ans;
     }
