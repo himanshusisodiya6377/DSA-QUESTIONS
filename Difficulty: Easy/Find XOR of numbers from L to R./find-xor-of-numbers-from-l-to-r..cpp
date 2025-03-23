5a958d0e3050d5af688fdc8585948ee1c3626bb9
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-23 09:44:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find XOR of numbers from L to R./find-xor-of-numbers-from-l-to-r..cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int xo(int n){
+        if(n%4==1) return 1;
+        else if(n%4==2) return n+1;
+        else if(n%4==3) return 0;
+        else return n;
+    }
+    int findXOR(int l, int r) {
+        int temp1=xo(l-1);
+        int temp2=xo(r);
+        return temp1^temp2;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        // Input
+
+        int l, r;
+        cin >> l >> r;
+
+        Solution obj;
+        cout << obj.findXOR(l, r) << endl;
+    
+cout << "~" << "\n";
+}
+}
+// } Driver Code Ends
