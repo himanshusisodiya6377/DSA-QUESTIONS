afe33fdebb961e607fd48273c38b4cf3ee3a7668
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-23 11:21:41 +0530

    Time: 3 ms (25.05%), Space: 7.8 MB (67.34%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     int minFlips(int a, int b, int c) {
         int ans=0;
-        while(a>0 || b>0 || c>0){
-            int x=a&1;
-            int y=b&1;
-            int z=c&1;
-            if(x==1 && y==1 && z==0) ans+=2;
-            if(x==0 && y==1 && z==0) ans+=1;
-            if(x==1 && y==0 && z==0) ans+=1;
-            if(x==0 && y==0 && z==1) ans+=1;
-            a=a>>1;
-            b=b>>1;
-            c=c>>1;
-        }
+        // while(a>0 || b>0 || c>0){
+        //     int x=a&1;
+        //     int y=b&1;
+        //     int z=c&1;
+        //     if(x==1 && y==1 && z==0) ans+=2;
+        //     if(x==0 && y==1 && z==0) ans+=1;
+        //     if(x==1 && y==0 && z==0) ans+=1;
+        //     if(x==0 && y==0 && z==1) ans+=1;
+        //     a=a>>1;
+        //     b=b>>1;
+        //     c=c>>1;
+        // }
+        int temp=a|b;
+        c=c^temp;
+        ans+=__builtin_popcount(c);
+        int check=a&b&c;
+        ans+=__builtin_popcount(check);
         return ans;
     }
 };
