c15c3a07771c118a018282b274ef2bda521c78cb
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-24 18:45:43 +0530

    Time: 2 ms (69.94%), Space: 97.3 MB (76.56%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -8,13 +8,15 @@ public:
         for (int i = 0; i < n; i++) {
             t ^= nums[i];
             int doit = 0;
-            for (int i = 0; i < maximumBit; i++) {
-                int temp = 1 << i;
-                int check = (t & temp);
-                if (check == 0) {
-                    doit = doit | (1 << i);
-                }
-            }
+            // for (int i = 0; i < maximumBit; i++) {
+            //     int temp = 1 << i;
+            //     int check = (t & temp);
+            //     if (check == 0) {
+            //         doit = doit | (1 << i);
+            //     }
+            // }
+            int temp=pow(2,maximumBit)-1;
+            doit=t^temp;
             ans[k] = doit;
             k--;
         }
