eb66f16bb351d5b86e5c3f040d07e7d50d1bc940
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-26 14:42:22 +0530

    Time: 50 ms (32.83%), Space: 90 MB (48.02%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        int r=grid.size();//rows
-        int c=grid[0].size();
-        vector<int>v;
-         for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-             v.push_back(grid[i][j]);
+        int r = grid.size(); // rows
+        int c = grid[0].size();
+        vector<int> v;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                v.push_back(grid[i][j]);
+            }
         }
-        }
-        sort(begin(v),end(v));        
-        int mididx=(r*c)/2;
-        int medi=v[mididx];
-        int ans=0;
-        for(int i=0;i<v.size();i++){
-            if(medi%x!=v[i]%x) return -1;
-             ans+=abs(medi-v[i])/x;
+        sort(begin(v), end(v));
+        int mididx = (r * c) / 2;
+        int medi = v[mididx];
+        int ans = 0;
+        for (int i = 0; i < v.size(); i++) {
+            if (medi % x != v[i] % x)
+                return -1;
+            ans += abs(medi - v[i]) / x;
         }
         return ans;
     }
