ae52d775e7c9a866e37ff1d98b635c097ba583fc
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-27 09:25:36 +0530

    Time: 0 ms (100%), Space: 19.9 MB (28.75%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,23 +2,38 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        // int count=0;
-        int max = n / 3;
-        int idx = INT_MIN;
-        vector<int> v;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[i] == nums[j])
-                    count++;
+        int count1=0;
+        int maxi1=0;
+        int count2=0;
+        int maxi2=0;
+       vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(nums[i]==maxi1) count1++;
+            else if(nums[i]==maxi2) count2++;
+            else if(count1==0){
+                maxi1=nums[i];
+                count1=1;
             }
-            if (count > max && nums[i] != idx) {
-                idx = nums[i];
-                v.push_back(idx);
+            else if(count2==0){
+                maxi2=nums[i];
+                count2=1;
+            }
+            else{
+                count1--;
+                count2--;
             }
         }
-
-        return v;
+      //first verification
+      int count=0;
+      for(int i=0;i<n;i++){
+        if(nums[i]==maxi1) count++;
+      }
+      if(count>n/3) ans.push_back(maxi1);
+      count=0;
+       for(int i=0;i<n;i++){
+        if(nums[i]==maxi2) count++;
+      }
+      if(count>n/3 && maxi1!=maxi2) ans.push_back(maxi2);
+    return ans;
     }
 };
