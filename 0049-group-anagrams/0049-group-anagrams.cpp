8b0fcf31e19af153183c16661e18f1cc701db1cb
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-27 22:22:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-     int n=strs.size();
-     unordered_map<string,vector<string>>mp;
-     vector<vector<string>>v;
-     for(int i=0;i<n;i++){
-        string ans=strs[i];
-        sort(begin(ans),end(ans));
-        mp[ans].push_back(strs[i]);
-     }
-     for(auto p : mp){
-        v.push_back(p.second);
-     }  
-     return v;
+        int n = strs.size();
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> v;
+        // for (int i = 0; i < n; i++) {
+        //     string ans = strs[i];
+        //     sort(begin(ans), end(ans));
+        //     mp[ans].push_back(strs[i]);
+        // }
+        // for (auto p : mp) {
+        //     v.push_back(p.second);
+        // }
+        for(auto num : strs){
+            vector<int>ap(26,0);
+            string ans="";
+            for(auto c : num) ap[c-'a']++;
+            for(int i=0;i<26;i++){
+                if(ap[i]!=0) ans.push_back(i+'a');
+            }
+            mp[ans].push_back(num);
+        }
+        for(auto num : mp){
+            v.push_back(num.second);
+        }
+        return v;
     }
 };
