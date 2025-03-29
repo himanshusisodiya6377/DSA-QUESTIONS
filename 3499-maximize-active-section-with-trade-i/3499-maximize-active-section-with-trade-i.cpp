bf635fdbc79134658cc0d8c119a806a79b440d21
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-29 21:41:43 +0530

    Time: 158 ms (30.04%), Space: 54.8 MB (97.45%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+     int n=s.length();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='1') ans++;
+        }
+        // int count=ans;
+        // for(int i=0;i<n;i++){
+        //     if(s[i]=='1') count--;
+        //     else if(count==0 && s[i]=='0') return ans; 
+        //     else break;
+        // }
+        // count=ans;
+        //  for(int i=n-1;i>=0;i--){
+        //     if(s[i]=='1') count--;
+        //     else if(count==0 && s[i]=='0') return ans;
+        //      else break;
+        // }
+        int k=0,m=0;
+        int count=0;
+        while(m<n){
+              if(s[k]=='1' && s[m]=='1'){
+                k++;
+                m++;
+            }
+            else if(s[k]=='0' && s[m]=='0') m++;
+            else{
+                count++;
+                k=m;
+            }
+        }
+        if(k!=m) count++;
+        if(count==1) return ans;
+        int first=0;
+        int second=0;
+        int maxi=INT_MIN;
+        int i=0,j=0;
+        while(j<n){
+            if(s[i]=='1' && s[j]=='1'){
+                i++;
+                j++;
+            }
+            else if(s[i]=='0' && s[j]=='0') j++;
+            else{
+                second=first;
+                first=(j-i);
+                if(first!=0 && second!=0){
+                int sum=first+second;
+                if(maxi<sum) maxi=sum;
+                }
+                i=j;
+            }
+        }
+         second=first;
+         first=(j-i);
+         int sum=first+second;
+         if(maxi<sum) maxi=sum;
+        ans+=maxi;
+        return ans;
+    }
+};
