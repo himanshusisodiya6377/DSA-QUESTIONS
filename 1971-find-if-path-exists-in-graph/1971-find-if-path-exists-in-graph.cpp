59318d1678a7fe6ee57aadc880758d38d185a143
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-29 10:23:31 +0530

    Time: 635 ms (20.55%), Space: 363 MB (19.36%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -12,21 +12,41 @@ public:
         }
     }
   }
-  bool check(int source,int dest,vector<int>&visit){
+//   bool dfs(int source,int dest,vector<int>&visit){
+//     if(source==dest) return true;
+//     visit[source]=1;
+//     for(auto neigh : ls[source]){
+//         if(visit[neigh]==0){
+//             bool result=dfs(neigh,dest,visit);
+//             if(result) return true;
+//         }
+//     }
+//     return false;
+//   }
+  bool bfs(int source,int dest,vector<int>&visit,queue<int>&q){
     if(source==dest) return true;
     visit[source]=1;
-    for(auto neigh : ls[source]){
+    q.push(source);
+   
+    while(!q.empty()){
+       int top=q.front();
+      q.pop();
+     for(int neigh : ls[top]){
+        if(neigh==dest) return true;
         if(visit[neigh]==0){
-            bool result=check(neigh,dest,visit);
-            if(result) return true;
+            q.push(neigh);
+            visit[neigh]=1;
         }
+     }
     }
     return false;
   }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<int>visit(n,0);
         creatls(edges,true);
-        bool flag=check(source,destination,visit);
+        queue<int>q;
+        bool flag=bfs(source,destination,visit,q);
+        // bool flag=dfs(source,destination,visit);
         return flag;
     }
 };
