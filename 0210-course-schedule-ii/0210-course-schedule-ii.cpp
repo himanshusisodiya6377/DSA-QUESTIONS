993cc97ab4dcf3b92e30778493ad9cb1849691dc
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-03-30 12:00:07 +0530

    Time: 0 ms (100%), Space: 18.6 MB (42.08%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> ls;
+    void creatls(vector<vector<int>>& prerequisites) {
+        int n = prerequisites.size();
+        for (int i = 0; i < n; i++) {
+            ls[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+    }
+    void khanalgo(int courses, vector<vector<int>>& prerequisites,
+                  vector<int>& visit, int& count,vector<int>&ans) {
+        vector<int> degree(courses, 0);
+        for (int i = 0; i < prerequisites.size(); i++) {
+            degree[prerequisites[i][1]]++;
+        }
+        queue<int> q;
+        for (int i = 0; i < courses; i++) {
+            if (degree[i] == 0) {
+                q.push(i);
+                visit[i] = 1;
+            }
+        }
+        // bfs
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+            ans.push_back(x);
+            count++;
+            for (auto neigh : ls[x]) {
+                if (visit[neigh] == 0) {
+                    degree[neigh]--;
+                    if (degree[neigh] == 0) {
+                        q.push(neigh);
+                        visit[neigh] = 1;
+                    }
+                }
+            }
+        }
+    }
+    vector<int>findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> visit(numCourses, 0);
+        vector<int>ans;
+        int count = 0;
+        creatls(prerequisites);
+        khanalgo(numCourses, prerequisites, visit, count,ans);
+        if(count!=numCourses) return {};
+        reverse(begin(ans),end(ans));
+       return ans;
+    }
+};
