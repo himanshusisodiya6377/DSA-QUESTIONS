16d2d4f6ba77aae4706a5f169857a2f0acfb20a3
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-01 21:57:00 +0530

    Time: 20 ms (38.72%), Space: 24.1 MB (100%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> v(n+1,0);
-        for (int i = 0; i < n; i++) {
-           v[nums[i]]++;
+       int n=nums.size();
+       vector<int>v;
+       int i=0;
+       while(i<n){
+        int coridx=nums[i]-1;
+        if(nums[coridx]==nums[i]) i++;
+        else swap(nums[i],nums[coridx]);
+       }
+       for(int i=0;i<n;i++){
+        if(nums[i]!=i+1){
+            v.push_back(nums[i]);
+            v.push_back(i+1);
         }
-        vector<int>ans;
-        int zero,twice;
-        for(int i=1;i<n+1;i++){
-            if(v[i]==0) zero=i;
-            if(v[i]==2) twice=i;
-        }
-        ans.push_back(twice);
-        ans.push_back(zero);
-        return ans;
+       }
+       return v;
     }
 };
