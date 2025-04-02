863fbd976a1877d74cda88dd823d0f04f7701f9f
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-02 19:25:32 +0530

    Time: 20 ms (21.44%), Space: 19.6 MB (11.84%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,13 +2,33 @@ class Solution {
 public:
     string removeDuplicates(string s) {
      int n=s.length();
-     string ans="";
-     for(int i=0;i<n;i++){
-        ans.push_back(s[i]);
-        if(ans.size()>=2){
-            if(ans[ans.size()-1]==ans[ans.size()-2]) ans.erase(ans.size()-2,2);
+    //  string ans="";
+    //  for(int i=0;i<n;i++){
+    //     ans.push_back(s[i]);
+    //     if(ans.size()>=2){
+    //         if(ans[ans.size()-1]==ans[ans.size()-2]) ans.erase(ans.size()-2,2);
+    //     }
+    //  }   
+
+    stack<pair<char,int>>st;
+    for(int i=0;i<n;i++){
+        if(st.size()>0 && st.top().first==s[i]){
+            st.top().second++;
+            if(st.top().second==2) st.pop();
+        } 
+        else{
+            st.push({s[i],1});
         }
-     }   
+    }
+    string ans="";
+    while(st.size()>0){
+         for(int i=0;i<st.top().second;i++){
+            ans.push_back(st.top().first);
+         }
+         st.pop();
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
      return ans;
     }
 };
