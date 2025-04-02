419f49df3b51f3661c0e732e2c52076f84a31cc1
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-02 19:36:52 +0530

    Time: 7 ms (50.76%), Space: 14.3 MB (38.1%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -10,25 +10,43 @@ public:
     //     }
     //  }   
 
-    stack<pair<char,int>>st;
+
+    //TC-O(n*m) SC-O(n)
+
+    // stack<pair<char,int>>st;
+    // for(int i=0;i<n;i++){
+    //     if(st.size()>0 && st.top().first==s[i]){
+    //         st.top().second++;
+    //         if(st.top().second==2) st.pop();
+    //     } 
+    //     else{
+    //         st.push({s[i],1});
+    //     }
+    // }
+    // string ans="";
+    // while(st.size()>0){
+    //      for(int i=0;i<st.top().second;i++){
+    //         ans.push_back(st.top().first);
+    //      }
+    //      st.pop();
+    // }
+    // reverse(ans.begin(),ans.end());
+    // return ans;
+
+    stack<char>st;
     for(int i=0;i<n;i++){
-        if(st.size()>0 && st.top().first==s[i]){
-            st.top().second++;
-            if(st.top().second==2) st.pop();
-        } 
+        if(st.size()>0 && st.top()==s[i]) st.pop();
         else{
-            st.push({s[i],1});
+            st.push(s[i]);
         }
     }
     string ans="";
     while(st.size()>0){
-         for(int i=0;i<st.top().second;i++){
-            ans.push_back(st.top().first);
-         }
+         char ch=st.top();
          st.pop();
+         ans.push_back(ch);
     }
     reverse(ans.begin(),ans.end());
     return ans;
-     return ans;
     }
 };
