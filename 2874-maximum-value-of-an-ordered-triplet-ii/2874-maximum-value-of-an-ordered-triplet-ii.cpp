b65e57fd44b97ad75cb78980ecd79001a3d36255
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-03 09:05:48 +0530

    Time: 3 ms (73.82%), Space: 91.1 MB (85.24%) - LeetHub

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -4,25 +4,39 @@ public:
         int n = nums.size();
         if (n <= 2)
             return 0;
-        vector<int> premax(n, nums[0]);
-        vector<int> suffmax(n, 0);
-        suffmax[n - 2] = nums[n - 1];
-        suffmax[n - 3] = max(nums[n - 2], nums[n - 1]);
-        for (int i = 1; i < n; i++) {
-            premax[i] = max(nums[i], premax[i - 1]);
+            //we have to maintain the nums[i] and nums[k] as max possible so we get premax and suffmax according to question
+            //for nums[j] we go to every index and get required sum
+            //TC-O(n) SC-O(n)
+
+        // vector<int> premax(n, nums[0]);
+        // vector<int> suffmax(n, 0);
+        // suffmax[n - 2] = nums[n - 1];
+        // suffmax[n - 3] = max(nums[n - 2], nums[n - 1]);
+        // for (int i = 1; i < n; i++) {
+        //     premax[i] = max(nums[i], premax[i - 1]);
+        // }
+        // for (int i = n - 4; i >= 0; i--) {
+        //     suffmax[i] = max(nums[i + 1], suffmax[i + 1]);
+        // }
+        // long long result = INT_MIN;
+        // for (int i = 0; i < n; i++) {
+        //     long long sum = (1LL * (premax[i] - nums[i])) * suffmax[i];
+        //     if (sum > result)
+        //         result = sum;
+        // }
+        // if (result < 0)
+        //     return 0;
+        // else
+        //     return result;
+
+        long long highest=0;
+        long long highest_diff=0;
+        long long answer=0;
+        for(int i=0;i<n;i++){
+            answer=max(answer,highest_diff*nums[i]);
+            highest_diff=max(highest_diff,highest-nums[i]);
+            highest=max(highest,(long long)nums[i]);
         }
-        for (int i = n - 4; i >= 0; i--) {
-            suffmax[i] = max(nums[i + 1], suffmax[i + 1]);
-        }
-        long long result = INT_MIN;
-        for (int i = 0; i < n; i++) {
-            long long sum = (1LL * (premax[i] - nums[i])) * suffmax[i];
-            if (sum > result)
-                result = sum;
-        }
-        if (result < 0)
-            return 0;
-        else
-            return result;
+        return answer;
     }
 };
