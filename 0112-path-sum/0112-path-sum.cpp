e4255572166a13d4f9bb65e07bf6734945b36738
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-04 09:19:44 +0530

    Time: 0 ms (100%), Space: 21.6 MB (34.86%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-void path(TreeNode*root,int target,int &x){
-   if(root==NULL) return;
-   if(root->val==target && root->left==NULL && root->right==NULL){
-    x=1;
-    return;
-   }
-   path(root->left,target-(root->val),x);
-   path(root->right,target-(root->val),x);
-   return;
+//our approach include a sum which increases so that when sum eqaul to target result in true
+//but its not feasible to handle if we use only target and subtract it every time with noda and pass it by value and when it reach the leaf node we can check whether we reach our target or not in this ways its easy to handle
+// void path(TreeNode*root,int target,int &x){
+//    if(root==NULL) return;
+//    if(root->val==target && root->left==NULL && root->right==NULL){
+//     x=1;
+//     return;
+//    }
+//    path(root->left,target-(root->val),x);
+//    path(root->right,target-(root->val),x);
+//    return;
+// }
+bool path(TreeNode*root,int target,int sum){
+    if(root==NULL) return false;
+    sum+=root->val;
+    if(sum==target && root->left==NULL && root->right==NULL) return true;
+    bool let=path(root->left,target,sum);
+    bool rig=path(root->right,target,sum);
+    if(let || rig) return true;
+    else return false;
 }
     bool hasPathSum(TreeNode* root, int targetSum) {
         int x=0;
-        path(root,targetSum,x);
-        if(x==1) return true;
-        else return false;
+       return path(root,targetSum,x);
+        // if(x==1) return true;
+        // else return false;
     }
 };
