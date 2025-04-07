301a2811fd005b1c4c68d46821bda53282b5980d
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-07 10:22:26 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.8%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -5,11 +5,15 @@ public:
     //         return true;
     //    long long int x=log2(abs((long long)n));
     //    return pow(2,x)==abs((long long)n);
-     long long int x=abs((long long )n);
-     if(n<4 && n!=1) return false;
-     while(n%4==0){
-        n/=4;
-     }
-     return n==1;
+    //TC-O(n)
+    //  long long int x=abs((long long )n);
+    //  if(n<4 && n!=1) return false;
+    //  while(n%4==0){
+    //     n/=4;
+    //  }
+    //  return n==1;
+    if(n==1) return true;
+    if(n<4 || n%4!=0) return false;
+    return isPowerOfFour(n/=4);
     }
 };
