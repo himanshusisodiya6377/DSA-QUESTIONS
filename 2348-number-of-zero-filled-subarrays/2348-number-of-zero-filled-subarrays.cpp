8c0cd3de5790046001a139330380887ee184582a
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-08 19:15:43 +0530

    Time: 1 ms (48.38%), Space: 111.3 MB (43.57%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -2,26 +2,38 @@ class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
      int n=nums.size();
-     vector<int>v;
-     int count=0;
+    //  //TC O(n) //SC O(1)
+     int c=0;
+     long long sum=0;
      for(int i=0;i<n;i++){
         if(nums[i]==0) {
-            count++;
+            c++;
         }
         else{
-            v.push_back(count);
-            count=0;
+            sum+=(0.5*c*(c+1));
+            c=0;
         }
      }   
-     v.push_back(count);
-     long long sum=0;
-     for(int i=0;i<v.size();i++){
-        if(v[i]!=0){
-            long long prod=(long long)v[i]*(v[i]+1);
-          sum+=(prod/2);
-        }
-        
-     }
+     if(c>0)  sum+=(0.5*c*(c+1));
      return sum;
+    //2nd approach
+    //TC-O(N) SC-O(N)
+    // int count=0;
+    // vector<int>pre(n,0);
+    // for(int i=0;i<n;i++){
+    // if(nums[i]==0){
+    //     count++;
+    //     pre[i]=count;
+    // }
+    // else{
+    //     count=0;
+    //     pre[i]=0;
+    // }
+    // }
+    // long long  sum=0;
+    // for(int i=0;i<n;i++){
+    //     sum+=pre[i];
+    // }
+    // return sum;
     }
 };
