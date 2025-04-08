61c8676a46ad426e895340979960c101caa1638f
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-08 17:43:04 +0530

    Time: 18 ms (86.4%), Space: 30 MB (86.55%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
-    bool isA(vector<int> arr) {
-        int n = arr.size();
-        int maxi = INT_MIN;
-        int mini = INT_MAX;
-        unordered_set<int> st;
-        for (int i = 0; i < n; i++) {
-            maxi = max(maxi, arr[i]);
-            mini = min(mini, arr[i]);
-            st.insert(arr[i]);
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        int c=l.size();
+        vector<bool>ans;
+        for(int i=0;i<c;i++){
+            bool flag=false;
+               vector<int>v(nums.begin()+l[i],nums.begin()+r[i]+1);
+               sort(v.begin(),v.end());
+               int size=r[i]-l[i]+1;
+               int diff=v[1]-v[0];
+               for(int i=2;i<size;i++){
+                if(v[i]-v[i-1]!=diff){
+                    ans.push_back(false);
+                    flag=true;
+                    break;
+                }
+               }
+               if(flag==false) ans.push_back(true);
         }
-        if (((maxi - mini) % (n - 1)) != 0)
-            return false;
-        int diff = (maxi - mini) / (n - 1);
-        while (mini < maxi) {
-            if (st.find(mini) == st.end())
-                return false;
-            mini += diff;
-        }
-        return true;
-    }
-    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l,
-                                          vector<int>& r) {
-        int c = l.size();
-        vector<bool> ans;
-        for (int i = 0; i < c; i++) {
-            vector<int> v(nums.begin() + l[i], nums.begin() + r[i] + 1);
-            bool flag = isA(v);
-            ans.push_back(flag);
-        }
-
         return ans;
     }
 };
