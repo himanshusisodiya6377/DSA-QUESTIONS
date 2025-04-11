14b02c9f801060cbd3e9cc0501a907e826e965cd
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-11 08:04:50 +0530

    Time: 83 ms (23.88%), Space: 10.2 MB (29.46%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.cpp
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
-int count(int i){
-    int count=0;
-    while(i>0){
-        count++;
-        i/=10;
+    // int count(int i) {
+    //     int count = 0;
+    //     while (i > 0) {
+    //         count++;
+    //         i /= 10;
+    //     }
+    //     return count;
+    // }
+    bool issymmetry(int n){
+        string s=to_string(n);
+        if(s.length()%2==1) return false;
+        int leftsum=0,rightsum=0;
+        for(int i=0;i<s.length()/2;i++){
+            leftsum+=s[i]-'0';
+            rightsum+=(s[s.length()-i-1]-'0');
+        }
+        return rightsum==leftsum;
     }
-    return count;
-}
     int countSymmetricIntegers(int low, int high) {
-        int ans=0;
+        int ans = 0;
+        // for (int i = low; i <= high; i++) {
+        //     int num = i;
+        //     int digit = count(num);
+        //     num = i;
+        //     if (digit % 2 != 0)
+        //         continue;
+        //     else {
+        //         int half = digit / 2;
+        //         int leftsum = 0, rightsum = 0;
+        //         while (num > 0 && half > 0) {
+        //             int x = num % 10;
+        //             rightsum += x;
+        //             num /= 10;
+        //             half--;
+        //         }
+        //         while (num > 0) {
+        //             int x = num % 10;
+        //             leftsum += x;
+        //             num /= 10;
+        //         }
+        //         if (leftsum == rightsum)
+        //             ans++;
+        //     }
+        // }
         for(int i=low;i<=high;i++){
-            int num=i;
-            int digit=count(num);
-            num=i;
-            if(digit%2!=0) continue;
-            else{
-                int half=digit/2;
-                int leftsum=0,rightsum=0;
-                while(num>0 && half>0){
-                    int x=num%10;
-                    rightsum+=x;
-                    num/=10;
-                    half--;
-                }
-                while(num>0){
-                    int x=num%10;
-                    leftsum+=x;
-                    num/=10;
-                }
-                if(leftsum==rightsum) ans++;
-            }
+            if(issymmetry(i)) ans++;
         }
         return ans;
     }
