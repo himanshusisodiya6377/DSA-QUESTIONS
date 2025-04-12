91676da2fa053c10410b21e9356cfc56335eaada
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-12 12:47:18 +0530

    Time: 1 ms (48.51%), Space: 15.7 MB (17.6%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
+    void helper(vector<int>&nums,vector<int>temp,int i){
+     if(i==nums.size()){
+        ans.push_back(temp);
+        return;
+     }
+     temp.push_back(nums[i]);
+     helper(nums,temp,i+1);
+     temp.pop_back();
+     helper(nums,temp,i+1);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
-        int total_subset = pow(2, n);
-        vector<vector<int>> ans;
-        // TC-O(n*2^n); with help of bit masking as input size is small
-        for (int i = 0; i < total_subset; i++) {
-            vector<int> v;
-            for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) != 0) {
-                    v.push_back(nums[j]);
-                }
-            }
-            ans.push_back(v);
-        }
+       vector<int>temp;
+       int i=0;
+        helper(nums,temp,i);
         return ans;
     }
 };
