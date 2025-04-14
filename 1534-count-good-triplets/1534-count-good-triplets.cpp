618d698552e3e69ab01df8b109f409be4de099e5
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-14 17:02:01 +0530

    Time: 3 ms (97.36%), Space: 11.1 MB (11.18%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.cpp
+++ 1534-count-good-triplets/1534-count-good-triplets.cpp
@@ -5,11 +5,12 @@ public:
         int n=arr.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
+                 int diff1=abs(arr[i] - arr[j]);
+                 if(diff1>a) continue;
                 for(int k=j+1;k<n;k++){
-                    int diff1=abs(arr[i] - arr[j]);
                     int diff2=abs(arr[j] - arr[k]);
                     int diff3=abs(arr[i] - arr[k]);
-                    if(diff1<=a && diff2<=b && diff3<=c) count++;
+                    if(diff2<=b && diff3<=c) count++;
                 }
             }
         }
