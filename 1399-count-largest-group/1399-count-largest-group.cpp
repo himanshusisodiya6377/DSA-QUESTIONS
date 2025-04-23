a0d1a8a0757363f52ce6fadef0408571e2e55a79
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-04-23 19:19:24 +0530

    Time: 0 ms (100%), Space: 8.2 MB (27.46%) - LeetHub

---
+++ 1399-count-largest-group/1399-count-largest-group.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int getsum(int n){
+        int sum=0;
+        while(n>0){
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+    int countLargestGroup(int n) {
+        unordered_map<int,int>mp;
+        for(int i=1;i<=n;i++){
+            int sum=getsum(i);
+            mp[sum]++;
+        }
+        int s=INT_MIN;
+        for(auto v : mp){
+            if(s<v.second) s=v.second;
+        }
+        int ans=0;
+        for(auto v : mp){
+            if(v.second==s) ans++;
+        }
+        return ans;
+    }
+};
