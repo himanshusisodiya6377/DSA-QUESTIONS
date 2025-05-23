253ae5227c5dd3ff1fad8979addf16740984de3c
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-05-23 22:09:27 +0530

    Time: 144 ms (13.57%), Space: 91.2 MB (11.8%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0) return 0;
-        set<int>st;
-        for(auto v: nums){
+        if (nums.size() == 0)
+            return 0;
+        set<int> st;
+        for (auto v : nums) {
             st.insert(v);
         }
-        int length=0;
-        vector<int>ans;
-        for(auto v :st){
-           ans.push_back(v);
-        }
-        int maxi=INT_MIN;
-        for(int i=0;i<ans.size()-1;i++){
-            if(ans[i]+1==ans[i+1]){
-                length++;
-               
+        int length = 0;
+        // vector<int>ans;
+        int prev = INT_MIN;
+        int maxi = INT_MIN;
+        for (auto v : st) {
+            if (prev == INT_MIN) {
+                prev = v;
+            } else {
+                if (prev + 1 == v) {
+                    length++;
+                    prev=v;
+                } else {
+                    maxi = max(maxi, length);
+                    length = 0;
+                    prev=v;
+                }
             }
-            else{
-                 maxi=max(maxi,length);
-                 length=0;
-            }
         }
-         maxi=max(maxi,length);
-        return maxi+1;
+
+        // for(int i=0;i<ans.size()-1;i++){
+        //     if(ans[i]+1==ans[i+1]){
+        //         length++;
+
+        //     }
+        //     else{
+        //          maxi=max(maxi,length);
+        //          length=0;
+        //     }
+        // }
+        maxi = max(maxi, length);
+        return maxi + 1;
     }
 };
