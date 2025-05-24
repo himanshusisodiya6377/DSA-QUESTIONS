d256d2382e1caafa59e6c85c231f20cefea1fc28
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-05-24 18:56:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (31.49%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-      int n=s.length();
-      int m=t.length();
-      if(n==0) return true;
-     int index=0;
-     for(int i=0;i<m;i++){
-        if(t[i]==s[0]){
-          index=i;
-          break;
-        } 
-     }
-     int count=0;
-     int j=0;
-     for(int i=index;i<m;i++){
-          if(s[j]==t[i]){
-            count++;
-            j++;
-          }
-     }
-     if(count==n) return true;
-     else return false;
+        // //nahi ho pa rha check karo yaar
+        // int n = a.length();
+        // int m = b.length();
+        // if(a=="") return true;
+        // if(b=="") return false;
+        // vector<vector<int>>t(n+1,vector<int>(m+1,0));
+        // for(int i=1;i<n+1;i++){
+        //     for(int j=1;j<m+1;j++){
+        //         if(a[i-1]==b[j-1]) t[i][j]=1+t[i-1][j-1];
+        //         else t[i][j]=max(t[i-1][j],t[i][j-1]);
+        //     }
+        // }
+        // return t[n][m]==n;
+        int n=s.length();
+        int m=t.length();
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(s[i]!=t[j]) j++;
+            else{
+                i++;
+                j++;
+            }
+        }
+        if(i>=n) return true;
+        else return false;
     }
 };
