2afb48d22af337b86182ceffbfb4ca8d4ceb7b40
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-05-26 11:11:45 +0530

    Time: 0 ms (100%), Space: 24 MB (17.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-       for(int i=0;i<n;i++){
-        bool flag=true;
-        for(int j=0;j<n-1-i;j++){
-            
-            if(nums[j]==0 && nums[j+1]!=0) {
-                swap(nums[j],nums[j+1]);
-                flag=false;
+        int n = nums.size();
+        //bubble sort
+        // for (int i = 0; i < n; i++) {
+        //     bool flag = true;
+        //     for (int j = 0; j < n - 1 - i; j++) {
+
+        //         if (nums[j] == 0 && nums[j + 1] != 0) {
+        //             swap(nums[j], nums[j + 1]);
+        //             flag = false;
+        //         }
+        //     }
+        //     if (flag == true)
+        //         break;
+        // }
+        int i=0,j=1;
+        while( i<n && j<n){
+            if(nums[i]==0 && nums[j]!=0){
+                swap(nums[i],nums[j]);
+                while(i<n && nums[i]!=0) i++;
+                while(j<n && nums[j]==0) j++;
             }
-           
+            else if(nums[i]==0 && nums[j]==0) j++;
+            else{
+                i++;
+                j++;
+            }
         }
-         if(flag==true) break;
-       }
+     
     }
 };
