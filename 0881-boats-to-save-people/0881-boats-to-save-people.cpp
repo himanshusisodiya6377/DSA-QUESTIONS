8f799618068afc6ad675353cd31fc4c36c7d7c76
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-05-26 10:26:23 +0530

    Time: 23 ms (21.46%), Space: 45.8 MB (36.34%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -3,22 +3,44 @@ public:
     int numRescueBoats(vector<int>& people, int limit) {
         int n=people.size();
         int ans=0;
-        vector<int>v(n,false);
+        // vector<int>v(n,false);
         sort(people.begin(),people.end());
-        for(int i=0;i<n-1;i++){
-            bool flag=false;
-            for(int j=n-1;j>i;j--){
-                if(people[i]+people[j]<=limit && (v[i]!=true && v[j]!=true)) {
-                    ans++;
-                    flag=true;
-                    v[i]=true;
-                    v[j]=true;
-                    break;
-                }
+        //tc-O(N^2) SC-O(N)
+        // for(int i=0;i<n-1;i++){
+        //     bool flag=false;
+        //     for(int j=n-1;j>i;j--){
+        //         if(people[i]+people[j]<=limit && (v[i]!=true && v[j]!=true)) {
+        //             ans++;
+        //             flag=true;
+        //             v[i]=true;
+        //             v[j]=true;
+        //             break;
+        //         }
+        //     }
+        //     if(flag==false && v[i]!=true) ans++;
+        // }
+        // if(v[n-1]==false) ans++;
+
+        //OPTIMISE
+        //TC-O(N) SC-O(1)
+        int i=0,j=n-1;
+        while(i<j){
+            int sum=people[i]+people[j];
+            // if(i==j){
+            //     ans++;
+            //     break;
+            // }
+            if(sum>limit){
+                j--;
+                ans++;
             }
-            if(flag==false && v[i]!=true) ans++;
+            else{
+                  i++;
+                  j--;
+                  ans++;
+            }
         }
-        if(v[n-1]==false) ans++;
+        if(i==j) ans++;
         return ans;
     }
 };
