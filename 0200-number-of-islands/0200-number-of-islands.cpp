e5b5770b307ed67c3c7f654eafd23f7ec368d07b
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-06-14 13:47:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-     void  dfs(vector<vector<char>>& grid, char target,vector<vector<int>>&visited, int r, int c, int row, int col) {
-        if (row < 0 || row == r || col < 0 || col == c || visited[row][col] == 1 || grid[row][col]!=target) {
-            return;
-        }
-        visited[row][col] = 1;
-        dfs(grid, target, visited, r, c, row + 1, col);
-        dfs(grid, target, visited, r, c, row - 1, col);
-        dfs(grid, target, visited, r, c, row, col - 1);
-        dfs(grid, target, visited, r, c, row, col + 1);
-    }
+ void dfs(unordered_map<int,vector<int>>&mp,unordered_set<int>&st,int start){
+          st.insert(start);
+
+          for(auto neigh : mp[start]){
+             if(!st.count(neigh)){
+                dfs(mp,st,neigh);
+             }
+          }
+   }
+   
     int numIslands(vector<vector<char>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
-       vector<vector<int>>visited(r,vector<int>(c,0));
-       int ans=0;
-        for (int i = 0; i < r ; i++) {
-            for(int j=0;j<c;j++){
-                if(grid[i][j]=='1' && visited[i][j]==0){
-                ans++;
-                dfs(grid,'1',visited,r,c,i,j);
+         unordered_map<int,vector<int>>mp;
+        int node=grid.size();
+        if(node==1 && grid[0][0]=='0') return 0;
+        for(int i=0;i<node;i++){
+            for(int j=0;j<node;j++){
+                if(grid[i][j]=='1' && i!=j){
+                    mp[i].push_back(j);
+                    mp[j].push_back(i);
                 }
+            }
+        }
+          unordered_set<int>st;
+        int ans=0;
+       
+        for(int i=0;i<node;i++){
+            if(!st.count(i)){
+             int start=i;
+            dfs(mp,st,start);
+            ans++;
             }
         }
         return ans;
