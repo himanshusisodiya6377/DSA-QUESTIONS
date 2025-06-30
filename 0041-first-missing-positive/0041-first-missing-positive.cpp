14dd6f3c34eb55d8279b9d51c7031863cdbca9fc
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-06-30 18:17:38 +0530

    Time: 0 ms (100%), Space: 55 MB (46.64%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,12 +2,27 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n=nums.size();
-        int i=1;
-        sort(nums.begin(),nums.end());
-        for(int m=0;m<n;m++){
-            if(nums[m]!=i) continue;
-            else i++;
+        //marking negative element to zero as we dont care about them
+        for(int i=0;i<n;i++){
+            if(nums[i]<0) nums[i]=0;
         }
-        return i;
+        //marking element position by making it negative
+        for(int i=0;i<n;i++){
+            int cord=abs(nums[i])-1;
+            if(cord<0 || cord>=n) continue;
+            else if(nums[cord]<0) continue;
+            else if(nums[cord]==0) nums[cord]=-(n+2);
+            else nums[cord]*=(-1);
+        }  
+
+        //checking in range 1 to n+1
+        for(int i=1;i<=n+1;i++){
+           int cord=i-1;
+           if(cord>=n) return i;
+           else if(nums[cord]<0) continue;
+           else return i;
+        }
+
+        return 0;
     }
 };
