efe4ee5e490096dfa5ef24200643e4d9d45d99c5
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-06-30 10:22:58 +0530

    Time: 10 ms (41.2%), Space: 19.4 MB (94.87%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -2,23 +2,32 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int n=nums.size();
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]]=3) mp[nums[i]]--;
-        }
+        // unordered_map<int,int>mp;
+        // for(int i=0;i<n;i++){
+        //     mp[nums[i]]++;
+        //     if(mp[nums[i]]=3) mp[nums[i]]--;
+        // }
         int i=0,j=0;
-        while(i<n && j<n){
-            if(mp.find(nums[j])!=mp.end()){
-                mp[nums[j]]--;
-                if(mp[nums[j]]==0) mp.erase(nums[j]);
-                if(i!=j) swap(nums[i],nums[j]);
+        // while(i<n && j<n){
+        //     if(mp.find(nums[j])!=mp.end()){
+        //         mp[nums[j]]--;
+        //         if(mp[nums[j]]==0) mp.erase(nums[j]);
+        //         if(i!=j) swap(nums[i],nums[j]);
+        //         i++;
+        //         j++;           
+        //     }
+        //     else j++;
+        // }
+        // int ele= nums[0];
+        for(auto ele : nums)
+        {
+            if(i==0 || i==1 || nums[i-2] != ele)
+            {
+                nums[i] = ele;
                 i++;
-                j++;           
             }
-            else j++;
         }
-   return i;
+    return i ;
     }
 
 };
