7fd4363c54557f6d953a40df9125841154717a6b
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-07-05 17:54:43 +0530

    Time: 5 ms (25.7%), Space: 20 MB (7.24%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -3,16 +3,27 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n=nums.size();
         unordered_map<int,int>mp;
-        priority_queue<pair<int,int>>pq;
+        // priority_queue<pair<int,int>>pq;
         for(int i=0;i<n;i++) mp[nums[i]]++;
         vector<int>ans;
+        // for(auto v : mp){
+        //   pq.push({v.second,v.first});
+        // }
+        // while(k>0){
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
+        //     k--;
+        // }
+        vector<vector<int>>visit(n+1);
         for(auto v : mp){
-          pq.push({v.second,v.first});
+            visit[v.second].push_back(v.first);
         }
-        while(k>0){
-            ans.push_back(pq.top().second);
-            pq.pop();
-            k--;
+        for(int i=n;i>=0 && k>0 ;i--){
+            if(visit[i].size()==0) continue;
+            for(int j=0;j<visit[i].size() && k>0;j++){
+                ans.push_back(visit[i][j]);
+                k--;
+            }
         }
         return ans;
     }
