3a796979669fdae1c0e8b4684efa85ab60af0d8b
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-07-11 11:16:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-     if(root==NULL){
-          TreeNode*c=new TreeNode(val);
-          return c;
-     } 
-     else if(root->left==NULL && root->val>val){
+     if(root->left==NULL || root->right==NULL){
         TreeNode*c=new TreeNode(val);
-        root->left=c;
+        if(root->val>val) root->left=c;
+        else root->right=c;
         return root;
      }
-     else if(root->right==NULL && root->val<val){
-         TreeNode*c=new TreeNode(val);
-        root->right=c;
-        return root;
-     }
-      else if(root->val>val) insertIntoBST(root->left,val);
+        if(root->val>val) insertIntoBST(root->left,val);
         else insertIntoBST(root->right,val);
-       
         return root;
     }
 };
