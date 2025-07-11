18140063462bbf5765ad9cf44811b58789ae31da
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-07-11 19:53:52 +0530

    Time: 8 ms (27.88%), Space: 27.1 MB (90.3%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,22 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-TreeNode*build(vector<int>pre,int prelo,int prehi,vector<int>in,int inlo,int inhi){
-    if(prehi<prelo) return NULL;
-    TreeNode*node=new TreeNode(pre[prelo]);
-    if(prelo==prehi) return node;
-    int i=inlo;
-    while(i<=inhi){
-        if(in[i]==pre[prelo]) break;
-        i++;
+
+    TreeNode*solve(vector<int>& inorder, vector<int>& preorder,int start,int end,int &idx){
+        if(start>end) return NULL;
+
+        int rootval=preorder[idx];
+        int i=start;
+
+        for(;i<=end;i++){
+            if(inorder[i]==rootval) break;
+        }
+
+        idx++;
+
+        TreeNode*root=new TreeNode(rootval);
+
+        root->left=solve(inorder,preorder,start,i-1,idx);
+        root->right=solve(inorder,preorder,i+1,end,idx);
+
+        return root;
+
     }
-    int leftcount=i-inlo;
-    int rightcount=inhi-i;
-    node->left=build(pre,prelo+1,prelo+leftcount,in,inlo,i-1);
-    node->right=build(pre,prelo+leftcount+1,prehi,in,i+1,inhi);
-    return node;
-}
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-     int n=preorder.size();
-     return build(preorder,0,n-1,inorder,0,n-1);   
+         int n=inorder.size();
+        int idx=0;
+        return solve(inorder,preorder,0,n-1,idx);
     }
 };
