3a419fbfbf4f387d55968fd7a157d06117849487
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-07-11 11:07:33 +0530

    Time: 0 ms (100%), Space: 21.7 MB (94.75%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    void helper(vector<int>&v,TreeNode* root){
-          if(root==NULL) return;
-          helper(v,root->left);
-          v.push_back(root->val);
-           helper(v,root->right);     
+
+    bool helper(TreeNode*root,long maxi,long mini){
+        if(root==NULL) return true;
+        if(root->val<=maxi || root->val>=mini) return false;
+        return helper(root->left,maxi,root->val) && helper(root->right,root->val,mini);
     }
+ 
     bool isValidBST(TreeNode* root) {
-        vector<int>v;
-        helper(v,root);
-        for(int i=0;i<v.size()-1;i++){
-            if(v[i]>=v[i+1]) return false;
-        }
-        return true;
+        return helper(root,LONG_MIN,LONG_MAX);
     }
 };
