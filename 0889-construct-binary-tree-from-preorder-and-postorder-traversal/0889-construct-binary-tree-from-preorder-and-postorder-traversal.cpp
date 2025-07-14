a77bbb3d6af2b99d39d3c947795586f8630ad60e
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-07-14 10:46:30 +0530

    Time: 0 ms (100%), Space: 28 MB (87.29%) - LeetHub

--- 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-TreeNode*build(vector<int>pre,int prelo,int prehi,vector<int>post,int postlo,int posthi){
-    if(prehi<prelo) return NULL;
-    TreeNode*node=new TreeNode(pre[prelo]);
-    if(prelo==prehi) return node;
-    int i=postlo;
-    while(i<=posthi){
-        if(post[i]==pre[prelo+1]) break;
-        i++;
+    TreeNode* build(vector<int>& pre,int prestart,int preend,
+               vector<int>& post,int poststart,int postend){
+
+        if(prestart>preend) return NULL;
+
+        int k=pre[prestart];
+        TreeNode* root=new TreeNode(k);  
+
+        if(prestart==preend) return root;
+
+        int left_root=pre[prestart+1];
+
+        int i=poststart;
+        while(i<=postend){
+            if(post[i]==left_root) break;
+            i++;
+        }  
+
+        int l=i-poststart+1;
+
+        root->left=build(pre, prestart+1, prestart+l, post, poststart, i);
+        root->right=build(pre, prestart+l+1, preend, post, i+1, postend-1);
+        return root;
     }
-    int leftcount=i-postlo+1;
-    node->left=build(pre,prelo+1,prelo+leftcount,post,postlo,postlo+leftcount);
-    node->right=build(pre,prelo+leftcount+1,prehi,post,i+1,posthi-1);
-    return node;
-}
-    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+
+
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post){
         int n=pre.size();
         return build(pre,0,n-1,post,0,n-1);
+        return NULL;
     }
 };
