aa1ddd1d23f0bf9f9091be3200561f48fa9062db
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-07-19 20:15:21 +0530

    Time: 0 ms (100%), Space: 7.6 MB (98.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-   
+   //memorization
    int climb(int n,vector<int>&dp){
         if(n<0) return 0;
         if(n==0) return 1;
@@ -9,7 +9,15 @@ public:
    }
  
     int climbStairs(int n) {
-       vector<int>dp(n+1,-1);
-       return climb(n,dp);
+    //    vector<int>dp(n+1,-1);
+    //    return climb(n,dp);
+    if(n==1) return 1;
+     int prev=2,prev2=1;
+    for(int i=3;i<=n;i++){
+        int sum=prev+prev2;
+        prev2=prev;
+        prev=sum;
+    }
+    return prev;
     }
 };
