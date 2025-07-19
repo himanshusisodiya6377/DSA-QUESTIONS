2f9d0ed7269e822972140ca7191b5a9ec0db9283
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-07-19 20:04:04 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int helper(unordered_map<int,int>&mp,int n){
-        if(n==0 || n==1) return 1;
-        if(mp.find(n)==mp.end()){
-            mp[n]=helper(mp,n-1)+helper(mp,n-2);
-        } 
-        return mp[n];
-    }
+   
+   int climb(int n,vector<int>&dp){
+        if(n<0) return 0;
+        if(n==0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=climb(n-1,dp)+climb(n-2,dp); 
+   }
+ 
     int climbStairs(int n) {
-        unordered_map<int,int>mp;
-        return helper(mp,n);
+       vector<int>dp(n+1,-1);
+       return climb(n,dp);
     }
 };
