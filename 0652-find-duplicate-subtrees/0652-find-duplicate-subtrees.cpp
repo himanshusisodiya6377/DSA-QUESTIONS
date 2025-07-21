271e2f171339d72747731d3c38e5c53203290175
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-07-21 15:22:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    string duplicate(TreeNode*root,unordered_map<string,int>&mp,vector<TreeNode*>&ans){
+        if(root==NULL) return "N";
+        string s=to_string(root->val)+","+duplicate(root->left,mp,ans)+","+  duplicate(root->right,mp,ans);
+        if(mp.find(s)!=mp.end() && mp[s]==1){
+            ans.push_back(root);
+        }
+        mp[s]++;
+        return to_string(root->val);
+    }
+
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        vector<TreeNode*>ans;
+        unordered_map<string,int>mp;
+        duplicate(root,mp,ans);
+        return ans;
+    }
+};
