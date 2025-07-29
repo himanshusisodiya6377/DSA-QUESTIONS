eaeaf3c9c9294e26b200865499610e125e59b47c
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-07-29 17:03:16 +0530

    Time: 3 ms (28.6%), Space: 11.1 MB (26.69%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    unordered_set<int>st;
+    void solve(vector<int>&temp,vector<int>&nums,vector<vector<int>>&result){
+        if(temp.size()==nums.size()){
+            result.push_back(temp);
+            return;
+        }
+        
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+             if(st.find(nums[i])==st.end()){
+                temp.push_back(nums[i]);
+                st.insert(nums[i]);
+                solve(temp,nums,result);
+                st.erase(nums[i]);
+                temp.pop_back();
+             }
+        }
+
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>temp;
+        vector<vector<int>>result;
+        solve(temp,nums,result);
+        return result;
+    }
+};
