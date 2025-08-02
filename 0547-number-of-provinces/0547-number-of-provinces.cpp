7e3c9ebbba2232c39924b5c28a35b1856407e49b
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-08-02 15:47:44 +0530

    Time: 8 ms (12.04%), Space: 21 MB (23.79%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> ls;
-    void creatls(vector<vector<int>>& isConnected, bool bi_dir) {
-        int n = isConnected.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1) {
-                    ls[i + 1].push_back(j + 1);
-                    ls[j + 1].push_back(i + 1);
+void dfs(int node,vector<int>&visit,vector<vector<int>>&adj){
+     
+     visit[node]=1;
+
+     for(auto it : adj[node]){
+      if(visit[it]==0){
+        dfs(it,visit,adj);
+      }
+     }
+
+   }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+
+        vector<vector<int>>adj(n);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j and isConnected[i][j]==1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
-    }
-    void dfs(vector<vector<int>>& isConnected, vector<int>& visit, int source) {
-        if (visit[source] == 1)
-            return;
-        visit[source] = 1;
-        for (auto neigh : ls[source]) {
-            if (visit[neigh] == 0) {
-                dfs(isConnected, visit, neigh);
-            }
-        }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> visit(n + 1, 0);
-        creatls(isConnected, true);
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (visit[i + 1] == 0) {
-                ans++;
-                dfs(isConnected, visit, i + 1);
-            }
-        }
-        return ans;
+
+          int cnt=0;
+          vector<int>visit(n,0);
+          for(int i=0;i<n;i++){
+         
+           if(visit[i]==0){
+            cnt++;
+            dfs(i,visit,adj);
+         }
+       }
+
+       return cnt;
     }
 };
