08485a7be4b0319a8f5e7d89d9ab33e21920d5e1
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-08-09 19:49:55 +0530

    Time: 8 ms (11.88%), Space: 19.6 MB (35.61%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -33,7 +33,17 @@ public:
     }
 
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
-        return profit(0,0,prices,dp);
+        vector<vector<int>>dp(prices.size(),vector<int>(2,INT_MIN));
+        dp[0][0]=0;
+        dp[0][1]=-prices[0];
+        for(int i=1;i<prices.size();i++){
+ 
+           dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i]);
+
+           dp[i][1]=max(dp[i-1][1],dp[i-1][0]-prices[i]);
+
+        }
+
+        return dp[prices.size()-1][0];
     }
 };
