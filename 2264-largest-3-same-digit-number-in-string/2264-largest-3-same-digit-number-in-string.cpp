ea91f6d8a107df7399a6fedebc182718df818f62
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-08-14 10:09:21 +0530

    Time: 0 ms (100%), Space: 9.1 MB (52.45%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -3,15 +3,15 @@ public:
     string largestGoodInteger(string num) {
         int n=num.length();
         string s="";
-        string ans="";
         for(int i=1;i<n-1;i++){
          if(num[i]==num[i+1] && num[i]==num[i-1]){
-            s+=num[i];
+            if(s.length()==0) s+=num[i];
+            else{
+                if(s[0]<num[i]) s[0]=num[i];
+            }
          }
         }
-        sort(s.begin(),s.end());
-        int m=s.length();
-        if(m!=0) for(int i=0;i<3;i++) ans+=s[m-1];
-        return ans;
+        if(s.length()!=0) for(int i=0;i<2;i++) s+=s[0];
+        return s;
     }
 };
