b1db0053598fa945450ee0f7a2a5cded40bfcf25
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-08-23 11:41:36 +0530

    Time: 5 ms (70.81%), Space: 20.9 MB (24.73%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,63 +1,62 @@
 class Solution {
 public:
-    int maxarea(vector<int>v){
-       int n=v.size();
-        vector<int>presmal(n,-1);
-        stack<int>st;
-        st.push(v[0]);
-        stack<int>idx;
-        idx.push(0);
-        for(int i=1;i<n;i++){//presmaller
-            while(st.size()>0 && st.top()>=v[i]){
-                st.pop();
-                idx.pop();
-            }
-            if(st.size()!=0){
-                presmal[i]=idx.top();
-            }
-            st.push(v[i]);
-            idx.push(i);
-        }
-        vector<int>nextsmal(n,n);
-        stack<int>sc;
-        sc.push(v[n-1]);
-        stack<int>idsc;
-        idsc.push(n-1);
-        for(int i=n-2;i>=0;i--){//nextsmaller
-            while(sc.size()>0 && sc.top()>=v[i]){
-                sc.pop();
-                idsc.pop();
-            }
-            if(sc.size()!=0){
-                nextsmal[i]=idsc.top();
-            }
-            sc.push(v[i]);
-            idsc.push(i);
-        }
-        int area=INT_MIN;
-        int sum;
-        for(int i=0;i<n;i++){
-            sum=v[i]*(nextsmal[i]-presmal[i]-1);
-            if(sum>area) area=sum;
-        }
+
+//presmall
+//nextsmall
+
+    int solve(vector<int>&heights){
+         int n=heights.size();
+         vector<int>pre(n,-1);
+         vector<int>next(n,n);
+
+         stack<int>presm;
+         presm.push(0);
+
+         for(int i=1;i<n;i++){
+            while(presm.size()>0 && heights[presm.top()]>=heights[i]) presm.pop();
+            if(presm.size()>0) pre[i]=presm.top();
+            presm.push(i);
+         }
+
+         stack<int>nextsm;
+         nextsm.push(n-1);
+
+         for(int i=n-2;i>=0;i--){
+           while(nextsm.size()>0 && heights[nextsm.top()]>=heights[i]) nextsm.pop();
+            if(nextsm.size()>0) next[i]=nextsm.top();
+            nextsm.push(i);
+            
+         }
+
+         int area=INT_MIN;
+
+         for(int i=0;i<n;i++){
+            int ar=heights[i]*((next[i]-pre[i])-1);
+            area=max(ar,area);
+         }
+        
         return area;
+
     }
+    
     int maximalRectangle(vector<vector<char>>& matrix) {
         
         int row=matrix.size();
         int col=matrix[0].size();
-        vector<int>v(col,0);
+
+        vector<int>ans(col,0);
+
         int maxi=INT_MIN;
+
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(matrix[i][j]=='1'){
-                    v[j]++;
-                }
-                else v[j]=0;
+                if(matrix[i][j]=='1') ans[j]++;
+                else ans[j]=0;
             }
-           int area=maxarea(v);
-           if(area>maxi) maxi=area;
+            maxi=max(maxi,solve(ans));
         }
+
         return maxi;
+
     }
 };
