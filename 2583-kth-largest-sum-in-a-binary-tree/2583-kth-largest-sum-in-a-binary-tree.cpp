180ca554d27c3690e6bd29e79403e14a9535e933
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-08-23 10:03:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+      
+    void solve(TreeNode*root,unordered_map<int,int>&mp,int level){
+        if(root==NULL) return;
+         
+        if(mp.find(level)==mp.end()) mp[level]=root->val;
+        else mp[level]+=root->val;
+
+        if(root->left!=NULL) solve(root->left,mp,level+1);
+        if(root->right!=NULL) solve(root->right,mp,level+1);
+        
+    }
+
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        unordered_map<int,int>mp;
+        solve(root,mp,1);
+        priority_queue<int>pq;
+
+        for(auto it : mp){
+            pq.push(it.second);
+        }
+
+        if(pq.size()<k) return -1;
+        else{
+            while(pq.size()>0 && k>1){
+                pq.pop();
+                k--;
+            }
+        }
+
+        return pq.top();
+    }
+};
