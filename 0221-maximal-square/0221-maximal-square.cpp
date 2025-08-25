4ddcda4589ef34f56362110a031f267db77a1bed
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-08-25 15:16:54 +0530

    Time: 9 ms (57.79%), Space: 31.1 MB (35.63%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -23,16 +23,38 @@ public:
      int c=matrix[0].size();
      int ans=0;
 
-     vector<vector<int>>dp(r,vector<int>(c,-1));
+    //  vector<vector<int>>dp(r,vector<int>(c,-1));
+
+    //  for(int i=0;i<r;i++){
+    //     for(int j=0;j<c;j++){
+    //         if(matrix[i][j]=='1'){
+    //             ans=max(ans,solve(i,j,matrix,dp));
+    //         }
+    //     }
+    //  }
+
+    //bottom up
+
+     vector<vector<int>>dp(r,vector<int>(c,0));
+
+     int maxi=0;
 
      for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
-            if(matrix[i][j]=='1'){
-                ans=max(ans,solve(i,j,matrix,dp));
+            if(i==0 || j==0){
+                if(matrix[i][j]=='1'){
+                dp[i][j]=1;
+                }
+                else dp[i][j]=0;
             }
+            else{
+                if(matrix[i][j]=='1')
+                dp[i][j]=1+min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]));
+            }
+            maxi=max(maxi,dp[i][j]);
         }
      }
 
-     return ans*ans;   
+     return maxi*maxi;   
     }
 };
