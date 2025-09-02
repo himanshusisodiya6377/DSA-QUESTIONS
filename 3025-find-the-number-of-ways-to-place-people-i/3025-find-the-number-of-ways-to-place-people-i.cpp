138249afb75819ffb51ab22c40b3110ef98d7f61
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-09-02 23:26:42 +0530

    Time: 0 ms (100%), Space: 32.7 MB (39.26%) - LeetHub

--- 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-
-    bool check(int i,int j,vector<vector<int>>&points){
-                int old_x = points[i][0];
-                int old_y = points[i][1];
-                int new_x = points[j][0];
-                int new_y = points[j][1];
-                for(int k=0;k<points.size();k++){
-                    if(k!=i && k!=j){
-                    int x=points[k][0];
-                    int y=points[k][1];
-                    if((x>=old_x && x<=new_x) && (y<=old_y && y>=new_y)) return false;
-                    }
-                }
-                return true;
+    static bool cmp(vector<int>&a,vector<int>&b){
+        if(a[0]!=b[0]) 
+        return a[0]<b[0];
+        return b[1]<a[1];
     }
-    int numberOfPairs(vector<vector<int>>& points) {
-        int n = points.size();
-        sort(points.begin(), points.end());
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            bool flag = false;
-            for (int j = i + 1; j < n; j++) {
 
-                int old_x = points[i][0];
-                int old_y = points[i][1];
-                int new_x = points[j][0];
-                int new_y = points[j][1];
-                if (flag == false && (new_x >= old_x && new_y <= old_y ||
-                                      new_x <= old_x && new_y >= old_y) && check(i,j,points)){
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(),points.end(),cmp);
+        int n=points.size();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            int min_y=-1;
+            int x=points[i][0];
+            int y=points[i][1];
+            for(int j=i+1;j<n;j++){
+                int new_x=points[j][0];
+                int new_y=points[j][1];
+                if(new_x>=x && new_y<=y && new_y>min_y){
                     cnt++;
-                flag = true;
-                                      }
-                else {
-                    if (new_x >= old_x && new_y == old_y)
-                        cnt++;
+                    min_y=max(min_y,new_y);
                 }
             }
         }
-    return cnt;
+        return cnt;
     }
 };
