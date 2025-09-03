8e00bc867c79dce465c0a9c20a94188612d9c3ae
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-09-03 09:15:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3027-find-the-number-of-ways-to-place-people-ii/3027-find-the-number-of-ways-to-place-people-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    static bool cmp(vector<int>&a,vector<int>&b){
+        if(a[0]!=b[0]) 
+        return a[0]<b[0];
+        return b[1]<a[1];
+    }
+
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(),points.end(),cmp);
+        int n=points.size();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            int min_y=-1;
+            int x=points[i][0];
+            int y=points[i][1];
+            for(int j=i+1;j<n;j++){
+                int new_x=points[j][0];
+                int new_y=points[j][1];
+                if(new_x>=x && new_y<=y && new_y>min_y){
+                    cnt++;
+                    min_y=max(min_y,new_y);
+                }
+            }
+        }
+        return cnt;
+    }
+};
