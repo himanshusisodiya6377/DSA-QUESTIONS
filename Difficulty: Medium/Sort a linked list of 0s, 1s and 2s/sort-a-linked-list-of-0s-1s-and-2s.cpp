9e6024bc137c0ab8f134650c6131bf26f36ffd51
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-09-08 01:07:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -10,33 +10,54 @@
     }
 };
 */
+Node* merge(Node*arr1,Node*arr2){
+       Node*temp1=arr1;
+       Node*temp2=arr2;
+       Node*c=new Node(-1);
+       Node*temp=c;
+       while(temp1!=NULL && temp2!=NULL){
+           if(temp1->data<=temp2->data){
+               temp->next=temp1;
+               temp1=temp1->next;
+               temp=temp->next;
+           }
+           else{
+               temp->next=temp2;
+               temp2=temp2->next;
+               temp=temp->next;
+           }
+       }
+       
+       while(temp1!=NULL){
+           temp->next=temp1;
+           temp1=temp1->next;
+           temp=temp->next;
+       }
+       
+       while(temp2!=NULL){
+           temp->next=temp2;
+           temp2=temp2->next;
+           temp=temp->next;
+       }
+       
+      return c->next;
+   }
 class Solution {
   public:
     Node* segregate(Node* head) {
         // code here
-        vector<int>v;
-        Node*temp=head;
-        while(temp!=NULL){
-            v.push_back(temp->data);
-            temp=temp->next;
+        if(!head || !head->next) return head;
+        
+        Node*slow=head;
+        Node*fast=head->next;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
         }
         
-        sort(v.begin(),v.end());
+        Node*temp=slow->next;
+        slow->next=NULL;
         
-        Node*c=new Node(-1);
-        
-        for(int i=0;i<v.size();i++){
-            Node*d=new Node(v[i]);
-            if(c->next==NULL){
-                c->next=d;
-                temp=d;
-            }
-            else{
-                temp->next=d;
-                temp=d;
-            }
-        }
-        
-        return c->next;
+        return merge(segregate(head),segregate(temp));
     }
 };
