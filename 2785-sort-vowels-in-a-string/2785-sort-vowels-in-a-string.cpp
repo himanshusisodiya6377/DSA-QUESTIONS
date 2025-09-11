344f9d1de9a14d523ff1e100fe6a63fa2b8552f1
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-09-11 08:57:22 +0530

    Time: 17 ms (43.62%), Space: 15.7 MB (78.32%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        string vowel="";
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='E' || s[i]=='A' || s[i]=='I' || s[i]=='O' || s[i]=='U' || s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+        vector<int>v(10,0);
+        string vowel = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'E' || s[i] == 'A' || s[i] == 'I' || s[i] == 'O' ||
+                s[i] == 'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
+                s[i] == 'o' || s[i] == 'u') {
                 vowel.push_back(s[i]);
             }
         }
-        sort(vowel.begin(),vowel.end());
-        int j=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='E' || s[i]=='A' || s[i]=='I' || s[i]=='O' || s[i]=='U' || s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
-                s[i]=vowel[j];
+        sort(vowel.begin(), vowel.end());
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'E' || s[i] == 'A' || s[i] == 'I' || s[i] == 'O' ||
+                s[i] == 'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
+                s[i] == 'o' || s[i] == 'u') {
+                s[i] = vowel[j];
                 j++;
-            } 
+            }
         }
         return s;
     }
