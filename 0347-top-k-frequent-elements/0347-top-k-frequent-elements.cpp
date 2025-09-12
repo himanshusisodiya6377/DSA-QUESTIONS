7ffdd4440237ae2f5a037a7852220fcc61cfa07e
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-09-13 00:24:19 +0530

    Time: 15 ms (5.14%), Space: 17.6 MB (99.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,29 +2,25 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n=nums.size();
-        unordered_map<int,int>mp;
-        // priority_queue<pair<int,int>>pq;
-        for(int i=0;i<n;i++) mp[nums[i]]++;
+        sort(nums.begin(),nums.end());
+        priority_queue<pair<int,int>>pq;
+        int i=0,j=0;
+        while(j<n){
+            while(j<n && nums[i]==nums[j]) j++;
+                int len=j-i;
+        cout<<"1"<<endl;
+                pq.push({len,nums[i]});
+                i=j;
+                j++;
+        }
+        if(i<n) pq.push({j-i,nums[i]});
         vector<int>ans;
-        // for(auto v : mp){
-        //   pq.push({v.second,v.first});
-        // }
-        // while(k>0){
-        //     ans.push_back(pq.top().second);
-        //     pq.pop();
-        //     k--;
-        // }
-        vector<vector<int>>visit(n+1);
-        for(auto v : mp){
-            visit[v.second].push_back(v.first);
+       
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
-        for(int i=n;i>=0 && k>0 ;i--){
-            if(visit[i].size()==0) continue;
-            for(int j=0;j<visit[i].size() && k>0;j++){
-                ans.push_back(visit[i][j]);
-                k--;
-            }
-        }
+        cout<<"1"<<endl;
         return ans;
     }
 };
