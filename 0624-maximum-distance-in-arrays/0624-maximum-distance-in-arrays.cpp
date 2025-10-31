dead424941c64aa26e5912a6bf6753e4627d3d32
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-10-31 17:07:44 +0530

    Time: 56 ms (5.49%), Space: 133.4 MB (5.49%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
+    typedef pair<int,int> p;
     int maxDistance(vector<vector<int>>& arrays) {
         // sort(arrays.begin(),arrays.end());
         int n=arrays.size();
-        vector<pair<int,int>>v;
+        // vector<pair<int,int>>v;
+        priority_queue<p>bg;
+        priority_queue<p,vector<p>,greater<p>>sm;
         for(int i=0;i<n;i++){
             vector<int>temp=arrays[i];
-            v.push_back({temp[0],i});
-            v.push_back({temp[temp.size()-1],i});
+            sm.push({temp[0],i});
+            bg.push({temp[temp.size()-1],i});
         }
-        sort(v.begin(),v.end());
-        int j=0,k=v.size()-1;
+        // sort(v.begin(),v.end());
+        // int j=0,k=v.size()-1;
         while(1){
-            if(v[j].second!=v[k].second){
-                return abs(v[j].first-v[k].first);
+            if(sm.top().second!=bg.top().second){
+                return abs(sm.top().first-bg.top().first);
             }
-            int diff1=abs(v[j+1].first-v[j].first);
-            int diff2=abs(v[k-1].first-v[k].first);
+            auto s=sm.top();
+            sm.pop();
+            auto b=bg.top();
+            bg.pop();
+            int diff1=abs(sm.top().first-s.first);
+            int diff2=abs(bg.top().first-b.first);
 
-            if(diff1<=diff2) j++;
-            else k--;
+            if(diff1<=diff2) {
+                // j++;
+                bg.push(b);
+            }
+            else {
+                // k--;
+                sm.push(s);
+            }
         }
         return 0;
     }
