d787b9c73bd5a65de88e01376f39dfd7a6ad9a45
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-11-01 10:29:12 +0530

    Time: 135 ms (13.06%), Space: 152.2 MB (10.46%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-      typedef pair<int,pair<int,int>> pp;
-    vector<vector<int>> kSmallestPairs(vector<int>& arr1, vector<int>& arr2, int k) {
-         int n=arr1.size();
-        int m=arr2.size();
-        //int i=0,j=0;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n=nums1.size();
+        int m=nums2.size();
         vector<vector<int>>ans;
-        priority_queue<pp,vector<pp>,greater<pp>>pq;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                pq.push({arr1[i]+arr2[j],{arr1[i],arr2[j]}});
-            }
+        priority_queue<pair<int,vector<int>>,vector<pair<int,vector<int>>>,greater<pair<int,vector<int>>>>pq;
+        pq.push({nums1[0]+nums2[0],{0,0}});
+            set<pair<int,int>> vis;
+            vis.insert({0,0});
+            
+        while(k>0 && !pq.empty()){
+          auto p=pq.top();
+          pq.pop();
+          k--;
+          int x=p.second[0];
+          int y=p.second[1];
+          ans.push_back({nums1[x],nums2[y]});
+        //   int sum1=INT_MIN;
+        //   int sum2=INT_MIN;
+          if(x<n-1 && !vis.count({x+1,y})){
+             int sum1=nums1[x+1]+nums2[y];
+             pq.push({sum1,{x+1,y}});
+             vis.insert({x+1,y});
+          }
+          if(y<m-1 && !vis.count({x,y+1})){
+            int sum2=nums1[x]+nums2[y+1];
+             pq.push({sum2,{x,y+1}});
+            vis.insert({x,y+1});
+          }       
+          
         }
-        
-        while(k--){
-            ans.push_back({pq.top().second.first,pq.top().second.second});
-            pq.pop();
-        }
-        // while(k--){
-        //     ans.push_back({arr1[i],arr2[j]});
-        //     cout<<arr1[i]<<" "<<arr2[j]<<endl;
-        //     if(i<n && j<m){
-        //         if(arr1[i+1]<arr2[j+1]) i++;
-        //         else j++;
-        //     }
-        // }
         return ans;
     }
 };
