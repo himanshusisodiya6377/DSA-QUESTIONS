707d2947a01db557b058dccc191e9d0b30a5743c
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-11-08 13:54:36 +0530

    Time: 147 ms (66.47%), Space: 172.9 MB (77.25%) - LeetHub

--- 3722-lexicographically-smallest-string-after-reverse/3722-lexicographically-smallest-string-after-reverse.cpp
+++ 3722-lexicographically-smallest-string-after-reverse/3722-lexicographically-smallest-string-after-reverse.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
-    typedef pair<char,int> pp;
     string lexSmallest(string s) {
         int n=s.length();
-        int i=0;
-        priority_queue<pp,vector<pp>,greater<pp>>pq;
-        for(int i=0;i<n;i++) pq.push({s[i],i});
-        while(!pq.empty()){
-            pair<char,int>p=pq.top();
-            pq.pop();
-            if(p.second==i){
-                i++;
-                continue;
-            }
-            else{
-                if(i==0){
-                    reverse(s.begin(),s.begin()+p.second+1);
-                    return s;
-                }
-                else{
-                    if(p.second!=s.length()-1){
-                        i++;
-                        continue;
-                    }
-                    else{
-                        reverse(s.end()-(p.second),s.end());
-                        return s;
-                    }
-                }
-            }
+        string ans=s;
+        for(int i=0;i<n;i++){
+            string temp=s;
+            reverse(temp.begin(),temp.begin()+i+1);
+            // cout<<temp<<endl;
+            if(temp<ans) ans=temp;
         }
-        return s;
+
+        for(int i=n-1;i>0;i--){
+            string temp=s;
+            reverse(temp.begin()+i,temp.end());
+            if(temp<ans) ans=temp;
+        }
+
+        return ans;
     }
 };
