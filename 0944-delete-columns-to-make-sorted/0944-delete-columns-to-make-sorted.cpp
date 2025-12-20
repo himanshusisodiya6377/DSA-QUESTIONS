842f3810b92e31df4733f02e7b867669553f2ad3
Himanshu Sisodiya <hsisodia988766@gmail.com>
2025-12-20 10:55:47 +0530

    Time: 3 ms (63.02%), Space: 15.5 MB (45.93%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -8,7 +8,7 @@ public:
             for(int j=0;j<n-1;j++){
                 if(strs[j][i]>strs[j+1][i]) {
                     count++;
-                    j+=m;
+                    break;
                 }
             }
         }
