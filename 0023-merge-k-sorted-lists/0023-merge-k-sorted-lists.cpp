4b2fe494fc50414aec9e9cdca46e86ffe92dbd82
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-01-24 10:27:24 +0530

    Time: 3 ms (66.18%), Space: 18.7 MB (39.14%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,39 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
- ListNode* merge(ListNode* list1, ListNode* list2) {
-   ListNode*c=new ListNode(-100);
-   ListNode*temp=c;
-   ListNode*temp1=list1;
-   ListNode*temp2=list2;
-   while(temp1!=NULL && temp2!=NULL){
-    if(temp1->val < temp2->val){
-        temp->next=temp1;
-        temp1=temp1->next;
-        temp=temp->next;
-    }
-    else{
-        temp->next=temp2;
-        temp2=temp2->next;
-        temp=temp->next;
-    }
-   
-   }
-   if(temp1==NULL) temp->next=temp2;
-   else temp->next=temp1;
- return c->next;
-    }
+    typedef pair<int,ListNode*> z;
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0) return NULL;
-        if(lists.size()==1) return lists[0];
-        while(lists.size()>1){
-          ListNode*a=lists[lists.size()-1];
-        lists.pop_back();
-        ListNode*b=lists[lists.size()-1];
-        lists.pop_back();
-        ListNode*c=merge(a,b);
-        lists.push_back(c);
+        int n=lists.size();
+
+        if(n==0) return NULL;
+        
+        ListNode*c=new ListNode(0);
+        ListNode*head=c;
+        
+        priority_queue<z,vector<z>,greater<z>>pq; //val idx
+        
+        for(int j=0;j<n;j++){
+            if(lists[j]!=NULL) pq.push({lists[j]->val,lists[j]});
         }
-       return lists[0];
+
+        // cout<<pq.size()<<endl;
+        
+        while(!pq.empty()){
+            auto p=pq.top();
+            pq.pop();
+            
+            int val=p.first;
+            ListNode*temp=p.second;
+            
+            c->next=temp;
+            c=temp;
+            // cout<<"yes";
+            if(temp->next) pq.push({temp->next->val,temp->next});
+        }
+        return head->next;
     }
-    
 };
