9bf69d1fb4b812fc91d12b8e0b57432af11cd214
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-01-29 13:50:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<bool> subsequenceSumAfterCapping(vector<int>& nums, int k) {
+
+          int n=nums.size();
+
+          sort(nums.begin(),nums.end());
+          unordered_set<int>st;
+          st.insert(0);
+
+          vector<bool>ans(n,false);
+          int idx=0;
+          //checking posibilities
+          for(int i=1;i<=n;i++){
+            //generating all sum
+              while(idx<n && nums[idx]<=i){
+                for(auto &it : st){
+                    int new_val=it+nums[idx];
+                    if(new_val<=k) st.insert(new_val);
+                }
+                idx++;
+              }
+
+              //if left me exist krta ho
+              if(st.find(k)!=st.end()){
+                ans[i-1]=true;
+              }
+              else{
+                // cout<<idx<<"idx"<<endl;
+                for(int j=idx;j<n;j++){
+                    int len=n-j;
+                    int needed=i*len;
+                    // cout<<needed<<k-needed<<endl;
+
+                    if(st.find(k-needed)!=st.end()){
+                        ans[i-1]=true;
+                        break;
+                    }
+                }
+              }
+          }  
+          return ans;
+    }
+};
