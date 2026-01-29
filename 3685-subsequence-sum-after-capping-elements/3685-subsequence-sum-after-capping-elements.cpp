c1454c8235efbdbee0f8ace5027baf6282c9d525
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-01-29 13:52:01 +0530

    Time: 1200 ms (18.65%), Space: 458.5 MB (21.43%) - LeetHub

--- 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.cpp
+++ 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.cpp
@@ -2,43 +2,44 @@ class Solution {
 public:
     vector<bool> subsequenceSumAfterCapping(vector<int>& nums, int k) {
 
-          int n=nums.size();
+        int n = nums.size();
 
-          sort(nums.begin(),nums.end());
-          unordered_set<int>st;
-          st.insert(0);
+        sort(nums.begin(), nums.end());
+        unordered_set<int> st;
+        st.insert(0);
 
-          vector<bool>ans(n,false);
-          int idx=0;
-          //checking posibilities
-          for(int i=1;i<=n;i++){
-            //generating all sum
-              while(idx<n && nums[idx]<=i){
-                for(auto &it : st){
-                    int new_val=it+nums[idx];
-                    if(new_val<=k) st.insert(new_val);
+        vector<bool> ans(n, false);
+        int idx = 0;
+        // checking posibilities
+        for (int i = 1; i <= n; i++) {
+            // generating all sum
+            while (idx < n && nums[idx] <= i) {
+                vector<int> curr(st.begin(), st.end());
+                for (int sum : curr) {
+                    int new_val = sum + nums[idx];
+                    if (new_val <= k)
+                        st.insert(new_val);
                 }
                 idx++;
-              }
+            }
 
-              //if left me exist krta ho
-              if(st.find(k)!=st.end()){
-                ans[i-1]=true;
-              }
-              else{
+            // if left me exist krta ho
+            if (st.find(k) != st.end()) {
+                ans[i - 1] = true;
+            } else {
                 // cout<<idx<<"idx"<<endl;
-                for(int j=idx;j<n;j++){
-                    int len=n-j;
-                    int needed=i*len;
+                for (int j = idx; j < n; j++) {
+                    int len = j-idx+1;
+                    int needed = i * len;
                     // cout<<needed<<k-needed<<endl;
 
-                    if(st.find(k-needed)!=st.end()){
-                        ans[i-1]=true;
+                    if (st.find(k - needed) != st.end()) {
+                        ans[i - 1] = true;
                         break;
                     }
                 }
-              }
-          }  
-          return ans;
+            }
+        }
+        return ans;
     }
 };
