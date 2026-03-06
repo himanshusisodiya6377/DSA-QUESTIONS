c050753caf3e04fdd6108ff13c727923dc6aa5c1
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-03-06 10:07:31 +0530

    Time: 38 ms (5.87%), Space: 112.1 MB (52.74%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
+    int mod=1e9+7;
     int numOfSubarrays(vector<int>& arr) {
         int n=arr.size();
-        int m=(int)1e9+7;
-        vector<int>sum(n,0);
-        sum[0]=arr[0];
-        for(int i=1;i<n;i++){
-            sum[i]=(arr[i]+sum[i-1]);
-        }
-        int odd=0,even=0;
+        
+        unordered_map<int,int>mp;
+        long long sum=0;
+        long long cnt=0;
         for(int i=0;i<n;i++){
-            if(sum[i]%2==0) even=(even+1)%m;
-            else odd=(odd+1)%m;
+            sum+=arr[i];
+            if(sum%2==0){
+               cnt+=mp[1];
+               cnt=cnt%mod;
+            }
+            else{
+               cnt+=mp[0];
+               cnt++;
+               cnt=cnt%mod;
+            }
+            mp[sum%2]++;
         }
-        //  odd = odd%m + (even%m * odd%m)%m;
-        odd = (odd % m + (int64_t(even) % m * int64_t(odd) % m) % m) % m;
-
-         return odd%m;
+        return cnt%mod;
     }
 };
