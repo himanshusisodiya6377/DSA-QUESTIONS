3f6f46766179229a7e90af26bc81dd70bc969e3c
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-03-10 10:42:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,32 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-void revpart(vector<int>&v,int i,int j){
-     reverse(v.begin()+i,v.begin()+j);
-}  
-ListNode* reverseKGroup(ListNode* head, int k) {
-        vector<int>v;
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head==NULL) return head;
         ListNode*temp=head;
-    while(temp!=NULL){
-        v.push_back(temp->val);
-        temp=temp->next;
-     }
-    //  int size=0;
-    //  temp=head;
-    //  while(temp!=NULL){
-    //     size++;
-    //     temp=temp->next;
-    //  }
-     for(int i=0;i<v.size();i+=k){
-        if((i+k)<=v.size()) revpart(v,i,i+k);
-     }
-     ListNode*c=new ListNode(-100);
-     ListNode*t=c;
-     for(int i=0;i<v.size();i++){
-        ListNode*test=new ListNode(v[i]);
-        t->next=test;
-        t=t->next;
-     }
-     return c->next;
+        int cnt=0;
 
+        while(temp!=NULL){
+            temp=temp->next;
+            cnt++;
+        }
+
+        if(cnt<k) return head;
+
+        ListNode*prev=NULL;
+        ListNode*curr=head;
+        ListNode*nxt=NULL;
+        cnt=0;
+
+        while(curr!=NULL && cnt<k){
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+            cnt++;
+        }
+
+        if(nxt!=NULL) head->next=reverseKGroup(nxt,k);
+
+        return prev;
     }
 };
