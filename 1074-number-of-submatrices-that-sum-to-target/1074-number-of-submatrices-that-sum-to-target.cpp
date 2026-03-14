5f3c6f229b254f433b07b200c448c3a1902fb0b9
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-03-14 10:55:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -14,31 +14,47 @@ public:
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 
-                long long curr=matrix[i][j]+(i>0 ? sum[i-1][j] : 0)+(j>0 ? sum[i][j-1] : 0)-(i>0 && j>0 ? sum[i-1][j-1] : 0);
+                long long curr=matrix[i][j]+(i>0 ? sum[i-1][j] : 0);
                 sum[i][j]=curr;
             }
         }
 
         // for(int i=0;i<r;i++){
         //     for(int j=0;j<c;j++){
-        //         cout<<sum[i][j]<<" ";
+        //         sum[i][j]+=(j>0 ? sum[i][j-1] : 0);
+        //     }
+        // }
+
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //       cout<<sum[i][j]<<" ";
         //     }
         //     cout<<endl;
         // }
         for(int r1=0;r1<r;r1++){
             for(int r2=r1;r2<r;r2++){
 
-                for(int c1=0;c1<c;c1++){
-                    for(int c2=c1;c2<c;c2++){
+                // for(int c1=0;c1<c;c1++){
+                //     for(int c2=c1;c2<c;c2++){
                        
-                       long long curr=sum[r2][c2]-(r1>0 ? sum[r1-1][c2] : 0)-(c1>0 ? sum[r2][c1-1] : 0)+ (r1>0 && c1>0 ? sum[r1-1][c1-1] : 0);
-                    //    cout<<r1<<" "<<c1<<" "<<r2<<" "<<c2<<" "<<curr<<endl;
-                       if(curr==target) ans++;
+                //        long long curr=sum[r2][c2]-(r1>0 ? sum[r1-1][c2] : 0)-(c1>0 ? sum[r2][c1-1] : 0)+ (r1>0 && c1>0 ? sum[r1-1][c1-1] : 0);
+                //     //    cout<<r1<<" "<<c1<<" "<<r2<<" "<<c2<<" "<<curr<<endl;
+                //        if(curr==target) ans++;
 
-                    //    mp[curr]++;
+                //     //    mp[curr]++;
 
-                    }
+                //     }
+                // }
+                unordered_map<int,int>mp;
+                mp[0]=1;
+                long long curr=0;
+                for(int j=0;j<c;j++){
+                   curr+=(sum[r2][j]-(r1>0 ? sum[r1-1][j] : 0));
+
+                   if(mp.find(curr-target)!=mp.end()) ans+=mp[curr-target];
+                   mp[curr]++;
                 }
+
             }
         }
 
