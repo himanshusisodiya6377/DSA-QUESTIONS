0350d054ba0ce4866a415a1c41f43efd80a3777c
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-03-24 10:30:56 +0530

    Time: 4 ms (29.22%), Space: 8.2 MB (46.48%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        
+        int i=0;
+
+        int x=0;
+
+        while(i<32){
+            // cout<<(n & (1<<i))<<endl;
+            if((n & (1<<i))>0){
+                x+=pow(2,31-i);
+            }
+            i++;
+            // cout<<x<<" "<<i<<endl;
+        }
+
+        return x;
+    }
+};
