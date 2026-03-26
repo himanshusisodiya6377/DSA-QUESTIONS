afe650cafeb676e9c8b39c24a72c34ef27ecb900
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-03-26 19:30:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int n = s.length();
-        int cnt_0 = 0, cnt_1 = 0;
+        int n=s.length();
 
-        int i = 0, j = 0;
-        int idx = -1;
-        int ans = 0;
-        while (j < n) {
+        int prev=0,strk=1,res=0;
 
-            (s[j] == '0' ? cnt_0++ : cnt_1++);
-
-            if (idx == -1)
-                idx = j;
-            else if (s[j - 1] != s[j])
-                idx = j;
-
-            if (cnt_0 != 0 && cnt_1 != 0 && cnt_0 == cnt_1) {
-                ans += cnt_0;
-                i = idx;
-                (s[i - 1] == '0' ? cnt_0 = 0 : cnt_1 = 0);
-            } else if (cnt_0 != 0 && cnt_1 != 0) {
-                if (j < n - 1) {
-                    if (s[j] != s[j + 1]) {
-                        ans+=min(cnt_0,cnt_1);
-                        i = idx;
-                        if (s[i - 1] == '0')
-                            cnt_0 = 0;
-                        else
-                            cnt_1 = 0;
-                    }
-                }
+        for(int i=1;i<n;i++){
+            if(s[i]==s[i-1]) strk++;
+            else{
+                prev=strk;
+                strk=1;
             }
-            j++;
+
+            if(strk<=prev) res++;
         }
 
-        // if (cnt_0 == cnt_1)
-            ans += min(cnt_0,cnt_1);
-
-        return ans;
+        return res;
     }
 };
