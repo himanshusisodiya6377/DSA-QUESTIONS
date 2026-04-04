640b8db41de9bb2d3c733a9b0c1dbbd09fe49508
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-04-04 11:37:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int n;
+    int ans=0;
     unordered_set<string>st;
-    int  solve(string &s,int idx){
-         if(idx>=s.length()) return 0;
+    void  solve(string &s,int idx,int count){
+        if (count + (s.length() - idx) <= ans) return;
+         if(idx>=s.length()){
+            ans=max(ans,count);
+            return;
+         }
          int maxi=0;
          for(int i=1;i<=n;i++){
             string temp=s.substr(idx,i);
             if(st.find(temp)==st.end()){
                 st.insert(temp);
-                maxi=max(maxi,1+solve(s,idx+i));
+                solve(s,idx+i,count+1);
                 st.erase(temp);
             }
          }
 
-         return maxi;
+        //  return count;
     }
     int maxUniqueSplit(string s) {
         n=s.length();
-        return solve(s,0);
+        solve(s,0,0);
+        return ans;
     }
 };
