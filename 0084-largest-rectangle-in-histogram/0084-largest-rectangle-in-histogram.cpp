aea037db6f7ed9b1451885b33b14799884fd08af
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-04-05 22:59:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,20 +1,45 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int>st;
         int n=heights.size();
-        if(n==1) return heights[0];
-        vector<int>v(n,0);
-        st.push(heights[n-1]);
-        for(int i=n-2;i>=0;--i){
-            while(st.size()>0 && st.top()<heights[i]) st.pop();
-            if(st.size()!=0) v[i]=heights[i];
+        vector<int>presmal(n,-1);
+        stack<int>st;
+        st.push(heights[0]);
+        stack<int>idx;
+        idx.push(0);
+        for(int i=1;i<n;i++){//presmaller
+            while(st.size()>0 && st.top()>=heights[i]){
+                st.pop();
+                idx.pop();
+            }
+            if(st.size()!=0){
+                presmal[i]=idx.top();
+            }
             st.push(heights[i]);
+            idx.push(i);
         }
-        int max=INT_MIN;
-          for(int i=0;i<v.size();i++){
-             if(max<v[i]) max=v[i];
+        vector<int>nextsmal(n,n);
+        stack<int>sc;
+        sc.push(heights[n-1]);
+        stack<int>idsc;
+        idsc.push(n-1);
+        for(int i=n-2;i>=0;i--){//nextsmaller
+            while(sc.size()>0 && sc.top()>=heights[i]){
+                sc.pop();
+                idsc.pop();
+            }
+            if(sc.size()!=0){
+                nextsmal[i]=idsc.top();
+            }
+            sc.push(heights[i]);
+            idsc.push(i);
+        }
+        int area=INT_MIN;
+        int sum;
+        for(int i=0;i<n;i++){
+            sum=heights[i]*(nextsmal[i]-presmal[i]-1);
+            if(sum>area) area=sum;
+        }
+        return area;
     }
-    return 2*max;
-   }
 };
