4107c1a97fd76acd98aaa6edf450c73f39fc9c63
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-05-28 22:19:52 +0530

    Time: 0 ms (100%), Space: 7.9 MB (47.8%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n < 4 && n!=1)
-            return false;
-    if(n==1) return true;
-    if(n%4!=0) return false;
-       long long int x=log2(abs((long long)n));
-       return pow(4,x/2)==abs((long long)n);
-    //TC-O(n)
-    //  long long int x=abs((long long )n);
-    //  if(n<4 && n!=1) return false;
-    //  while(n%4==0){
-    //     n/=4;
-    //  }
-    //  return n==1;
-    //recursive approach
-    //TC-O(n) SC-O(n)
-    // if(n==1) return true;
-    // if(n<4 || n%4!=0) return false;
-    // return isPowerOfFour(n/=4);
+        if(n<0) return false;
+        if(n==0) return false;
+        int power=log2(n);
+        if(pow(2,power)!=n) return false;
+        return power%2==0;
     }
 };
