522d22f178e22f80a7a415816dd9a9af598c1a83
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-05-31 00:37:02 +0530

    Time: 71 ms (15.08%), Space: 28.1 MB (11.6%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<string>v;
+        for(int i=1;i<=n;i++){
+            v.push_back(to_string(i));
+        }
+        vector<int>ans;
+        sort(v.begin(),v.end());
+        for(auto &it : v){
+            ans.push_back(stoi(it));
+        }
+        return ans;
+    }
+};
