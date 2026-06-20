0db706690b1b53c18ffc3a57d6dc3762b8f1147c
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-06-20 19:52:01 +0530

    Time: 56 ms (47.86%), Space: 96.8 MB (35.21%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -9,36 +9,44 @@ public:
         // for(auto &it : items){
         //     cout<<it[0]<<" "<<it[1]<<endl;
         // }
-        unordered_map<int,vector<int>>mp;
+        // unordered_map<int,vector<int>>mp;
+        vector<pair<int,int>>v;
 
-        for(int i=0;i<queries.size();i++) mp[queries[i]].push_back(i);
-        sort(queries.begin(),queries.end());
+        for(int i=0;i<queries.size();i++){
+            v.push_back({queries[i],i});
+        }
 
-        int maxi=0;
+        // for(int i=0;i<queries.size();i++) mp[queries[i]].push_back(i);
+        sort(v.begin(),v.end());
+        priority_queue<int>pq;
+        // int maxi=0;
         int i=0,j=0;
 
         while(i<n && j<queries.size()){
             
-            while(i<n && items[i][0]<=queries[j]){
-                maxi=max(maxi,items[i][1]);
+            while(i<n && items[i][0]<=v[j].first){
+                // maxi=max(maxi,items[i][1]);
+                pq.push(items[i][1]);
                 i++;
             }
             // i++;
-            for(auto &it : mp[queries[j]]){
-            ans[it]=maxi;
-            }
+            // for(auto &it : mp[queries[j]]){
+            // ans[it]=maxi;
+            // }
             // cout<<i<<" "<<queries[j]<<" "<<maxi<<endl;
+            ans[v[j].second]=(pq.empty() ? 0 : pq.top());
             j++;
-            while(j<queries.size() && queries[j]==queries[j-1]) j++;
+            // while(j<queries.size() && queries[j]==queries[j-1]) j++;
             // j++;
         }
 
         while(j<queries.size()){
-            for(auto &it : mp[queries[j]]){
-            ans[it]=maxi;
-            }
+            // for(auto &it : mp[queries[j]]){
+            // ans[it]=maxi;
+            // }
+             ans[v[j].second]=(pq.empty() ? 0 : pq.top());
             j++;
-             while(j<queries.size() && queries[j]==queries[j-1]) j++;
+            //  while(j<queries.size() && queries[j]==queries[j-1]) j++;
         }
         return ans;
     }
