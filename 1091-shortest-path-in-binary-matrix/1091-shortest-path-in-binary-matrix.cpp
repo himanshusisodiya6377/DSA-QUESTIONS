7319127c832be3c8b8452656265f9fca944bb080
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-06-26 16:34:00 +0530

    Time: 83 ms (12%), Space: 42.8 MB (9.75%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
-
-    typedef pair<int,pair<int,int>> p;
-
-     bool issafe(int x,int y,vector<vector<int>>& grid){
-        if((x>=0 && x<grid.size()) && (y>=0 && y<grid[0].size())) return true;
-        return false;
-    }
-  
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-       int r=grid.size();
-       int c=grid[0].size();
-       if(r==1 && c==1 && grid[r-1][c-1]==0) return 1; 
-       if(grid[0][0]==1 || grid[r-1][c-1]==1) return -1;
-       vector<vector<int>>dist(r,vector<int>(c,INT_MAX));
-       //mark it 1
-       dist[0][0]=0;
-       vector<pair<int,int>>path={{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{-1,1},{1,-1}};
-       priority_queue<p,vector<p>,greater<p>>pq;
-       pq.push({1,{0,0}});
+        int n = grid.size();
 
-       while(!pq.empty()){
-        auto v =pq.top();
-        pq.pop();
-        int x=v.second.first;
-        int y=v.second.second;
-        int wt=v.first;
+        if (grid[0][0] == 1 || grid[n-1][n-1] == 1)
+            return -1;
 
-        for(auto dir : path){
-            int _x=x+dir.first;
-            int _y=y+dir.second;
+        queue<vector<int>> q;   // {row, col, distance}
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
 
-            if(issafe(_x,_y,grid) && grid[_x][_y]==0 && (wt+1<dist[_x][_y])){
-                dist[_x][_y]=wt+1;
-                pq.push({dist[_x][_y],{_x,_y}});
-                // grid[_x][_y]=1;
+        int dr[] = {-1,-1,-1,0,0,1,1,1};
+        int dc[] = {-1,0,1,-1,1,-1,0,1};
+
+        q.push({0, 0, 1});
+        vis[0][0] = true;
+
+        while (!q.empty()) {
+            auto cur = q.front();
+            q.pop();
+
+            int r = cur[0];
+            int c = cur[1];
+            int dis = cur[2];
+
+            if (r == n - 1 && c == n - 1)
+                return dis;
+
+            for (int i = 0; i < 8; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n &&
+                    !vis[nr][nc] && grid[nr][nc] == 0) {
+
+                    vis[nr][nc] = true;
+                    q.push({nr, nc, dis + 1});
+                }
             }
         }
-       }
-       //here creating problem
-       return dist[r-1][c-1] == INT_MAX ? -1 : dist[r-1][c-1];
 
+        return -1;
     }
 };
