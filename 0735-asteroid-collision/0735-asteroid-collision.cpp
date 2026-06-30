00fe71e89122178389fc6ef2409ce69210b5d2cc
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-06-30 15:21:50 +0530

    Time: 0 ms (100%), Space: 22.2 MB (18.79%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        int n=asteroids.size();
+    vector<int> asteroidCollision(vector<int>& ast) {
+        int n=ast.size();
+
         stack<int>st;
+
         for(int i=0;i<n;i++){
-           if(st.size()==0) st.push(asteroids[i]);
-           else if(st.top()>0 && asteroids[i]>0) st.push(asteroids[i]);
-           else if(st.top()<0 && asteroids[i]<0) st.push(asteroids[i]);
-           else if(st.top()<0 && asteroids[i]>0) st.push(asteroids[i]);
-           else{
-            if(st.top()>abs(asteroids[i])) continue;
+            if(st.empty()) st.push(ast[i]);
+            else if(ast[i]>0) st.push(ast[i]);
             else{
-                while(st.size()>0){
-                    if(st.top()>0 && st.top()<abs(asteroids[i])) st.pop();
-                    else if(st.top()>0 && st.top()==abs(asteroids[i])) {
-                        st.pop();
-                        break;
-                    }
-                    else if(st.top()>0 && st.top()>abs(asteroids[i])) break;
-                    else{
-                        st.push(asteroids[i]);
-                        break;
-                    }
+                //we have negartive
+                // if top is also negative 
+                if(!st.empty() && st.top()<0) st.push(ast[i]);
+                else{
+                    while(!st.empty() && st.top()>0 && st.top()<abs(ast[i])) st.pop();
+                    if(st.empty() || st.top()<0) st.push(ast[i]);
+                    if(!st.empty() && st.top()>0 && st.top()==abs(ast[i])) st.pop();
                 }
             }
-           }
         }
+
         vector<int>ans;
-        while(st.size()>0){
+        while(!st.empty()){
             ans.push_back(st.top());
             st.pop();
         }
