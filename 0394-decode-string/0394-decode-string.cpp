3b03d56767093027d01d6ff7e7b90348f609b2e6
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-06-30 17:47:15 +0530

    Time: 0 ms (100%), Space: 9.5 MB (33.72%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,65 +1,49 @@
 class Solution {
 public:
     string decodeString(string s) {
-        int n=s.length();
-        stack<string>st;
-         
-        string ans="";
+        stack<string> st;
+        stack<int> nume;
 
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='['){
-                 string temp="";
-                 while(!st.empty() && st.top()!="]"){
-                    temp +=st.top();
+        int num = 0;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            else if (c == '[') {
+                nume.push(num);
+                num = 0;
+                st.push("[");
+            }
+            else if (c == ']') {
+                string curr = "";
+
+                while (!st.empty() && st.top() != "[") {
+                    curr = st.top() + curr;
                     st.pop();
-                 }
-                //  cout<<"h"<<endl;
-                //  cout<<st.size()<<endl;
-                 st.pop();
-                //  cout<<"nikal"<<endl;
-                 i--;
-                 int k=0;
-                //  cout<<i<<endl;
-                 stack<char>num;
-                 while(i>=0 && isdigit(s[i])){
-                    //   int x=s[i]-48;
-                    //   k+=x;
-                    //   k*=10;
-                    num.push(s[i]);
-                    // cout<<"yes"<<endl;
-                    i--;
-                 }
-                 i++;
+                }
 
-                 while(!num.empty()){
-                    int x=num.top()-'0';
-                    // cout<<x;
-                    // k+=x;
-                    k=k*10+x;
-                    num.pop();
-                 }
-                 string copy=temp;
-                //  k/=10;
-                // cout<<k<<endl;
-                 while(k>1) {
-                    temp+=copy;
-                    // cout<<temp<<endl;
-                    k--;
-                 }
-                //  cout<<temp<<endl;
-                 st.push(temp);
+                st.pop(); // remove '['
+
+                int k = nume.top();
+                nume.pop();
+
+                string temp = "";
+                while (k--) temp += curr;
+
+                st.push(temp);
             }
-            else{
-                st.push(string(1, s[i]));
-                // cout<<st.top()<<endl;
+            else { // character
+                st.push(string(1, c));
             }
         }
-      
-        while(!st.empty()){
-            ans+=st.top();
+
+        string res = "";
+        while (!st.empty()) {
+            res = st.top() + res;
             st.pop();
-            // cout<<"yes"<<endl;
         }
-        return ans;
+
+        return res;
     }
 };
