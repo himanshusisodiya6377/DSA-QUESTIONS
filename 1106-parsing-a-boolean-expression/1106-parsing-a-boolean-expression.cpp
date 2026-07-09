5a57e0f7d877a6babf5c9ff4024918da3ba5bde6
Himanshu Sisodiya <hsisodia988766@gmail.com>
2026-07-09 18:48:44 +0530

    Time: 2 ms (65.61%), Space: 11.1 MB (40.77%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -1,87 +1,48 @@
 class Solution {
 public:
-    bool solve(queue<char>& q, char ch) {
-        bool flag;
-        if (ch == '&') {
-            flag = true;
-            while (!q.empty()) {
-                flag = flag && (q.front() == 'f' ? false : true);
-                q.pop();
-            }
-        } else if (ch == '|') {
-            flag = false;
-            while (!q.empty()) {
-                flag = flag || (q.front() == 'f' ? false : true);
-                q.pop();
-            }
-        }
-        return flag;
-    }
-    bool sol(char fir) {
-        if (fir == 'f')
-            return true;
-        return false;
-    }
     bool parseBoolExpr(string exp) {
-        int n = exp.length();
+        int n=exp.length();
+        int i=n-1;
 
-        stack<char> st;
-        stack<char> op;
+        stack<char>st;
 
-        for (int i = 0; i < n; i++) {
-            if (exp[i] == 'f')
-                st.push(exp[i]);
-            else if (exp[i] == 't')
-                st.push(exp[i]);
-            else if (exp[i] == '&' || exp[i] == '|' || exp[i] == '!')
-                op.push(exp[i]);
-            else if (exp[i] == '(')
-                st.push(exp[i]);
-            else if (exp[i] == ')') {
-                char ch = op.top();
-                // cout<<ch<<endl;
-                op.pop();
+        while(i>=0){
+            if(exp[i]=='f' || exp[i]=='t') st.push(exp[i]);
+            else if(exp[i]==')') st.push(exp[i]);
+            else if(exp[i]=='('){
+                //we had a openinng char
 
-                if (ch == '!') {
-                    // op.pop();
-                    char fir = st.top();
-                    st.pop();
-                    st.pop(); // for '('
-                    bool res = sol(fir);
-                    // cout<<res<<endl;
-                    st.push((res == false ? 'f' : 't'));
+                int ope=exp[i-1];
+                if(ope=='!'){
+                   char ch=st.top();
+                   st.pop();
+                   if(!st.empty()) st.pop();
+                   (ch=='f' ? st.push('t') : st.push('f'));
+                }
+                else if(ope=='&'){
+                    bool flag=true;
+                   while(!st.empty() && st.top()!=')'){
+                     char ch=st.top();
+                     st.pop();
+                     flag &= (ch=='f' ? false : true);
+                   }
+                   st.pop();
+                   st.push(flag ? 't' : 'f');
                 }
                 else{
-                queue<char> q;
-                while (st.size() > 0 && st.top() != '(') {
-
-                    char fir = st.top();
-                    st.pop();
-                    q.push(fir);
-                }
-
-                st.pop();
-
-                bool res = solve(q, ch);
-
-                // cout<<res<<endl;
-
-                (res == false ? st.push('f') : st.push('t'));
+                   bool flag=false;
+                   while(!st.empty() && st.top()!=')'){
+                     char ch=st.top();
+                     st.pop();
+                     flag |= (ch=='f' ? false : true);
+                   }
+                   st.pop();
+                   (flag==false ? st.push('f') : st.push('t'));
                 }
             }
+            i--;
         }
 
-        // cout<<st.top();
-        // cout<<op.size()<<endl;
-        while (!op.empty()) {
-            op.pop();
-            char fir = st.top();
-            st.pop();
-            bool res = sol(fir);
-            // cout<<res<<endl;
-            st.push((res == false ? 'f' : 't'));
-        }
-
-        return (st.top() == 'f' ? false : true);
+        return st.top()=='f' ? false : true;
     }
 };
